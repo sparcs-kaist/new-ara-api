5aba59a435fc20829f2f9f02e9f6faa744e79f73
workwithraon <workwithraon@gmail.com>
2017-09-14 04:42:32 +0900

    Add PageNumberPagination class and apply to the django-rest-framework settings

---
+++ ara/ara/classes/pagination.py
@@ -0,0 +1,16 @@
+from collections import OrderedDict
+
+from rest_framework import response, pagination
+
+
+class PageNumberPagination(pagination.PageNumberPagination):
+    page_size = 25
+    page_size_query_param = 'page_size'
+
+    def get_paginated_response(self, data):
+        return response.Response(OrderedDict([
+            ('num_pages', self.page.paginator.num_pages),
+            ('num_items', self.page.paginator.count),
+            ('current', self.page.number),
+            ('results', data)
+        ]))
