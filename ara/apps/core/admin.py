93804f4fef8bbe9ba122ef928dd8fd976831aa73
talkwithraon <talkwithraon@gmail.com>
2017-09-14 08:02:48 +0900

    Add Article admin

--- ara/apps/core/admin.py
+++ ara/apps/core/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 from ara.classes.admin import MetaDataModelAdmin
 
-from apps.core.models import Board, Topic
+from apps.core.models import Board, Topic, Article
 
 
 @admin.register(Board)
@@ -28,6 +28,35 @@ class TopicAdmin(MetaDataModelAdmin):
     search_fields = (
         'ko_name',
         'en_name',
+        'ko_description',
+        'en_description',
+    )
+
+
+@admin.register(Article)
+class ArticleAdmin(MetaDataModelAdmin):
+    list_filter = (
+        'is_anonymous',
+        'is_content_sexual',
+        'is_content_social',
+        'parent_topic',
+        'parent_board',
+    )
+    list_display = (
+        'title',
+        'hit_count',
+        'positive_vote_count',
+        'negative_vote_count',
+        'is_anonymous',
+        'is_content_sexual',
+        'is_content_social',
+        'created_by',
+        'parent_topic',
+        'parent_board',
+    )
+    search_fields = (
+        'title',
+        'content',
         'ko_description',
         'en_description',
     )
