bc345a5f04aaf8c380baad4cdf18c1650f69edbf
talkwithraon <talkwithraon@gmail.com>
2017-09-14 03:50:35 +0900

    Set up project default settings

--- ara/ara/settings/settings.py
+++ ara/ara/settings/settings.py
@@ -103,15 +103,15 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ko-kr'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
