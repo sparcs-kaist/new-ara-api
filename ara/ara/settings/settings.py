d9b7733f107b39510b585b2b8827c23b0e257a6d
talkwithraon <talkwithraon@gmail.com>
2017-09-14 04:05:09 +0900

    Install celery and set up

--- ara/ara/settings/settings.py
+++ ara/ara/settings/settings.py
@@ -118,3 +118,8 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# http://docs.celeryproject.org/
+
+CELERY_BROKER_URL = 'redis://localhost:6379/0'
