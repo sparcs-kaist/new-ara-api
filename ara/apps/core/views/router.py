738bb74e7f65a8c61eb915e9a543405ea7419d32
talkwithraon <talkwithraon@gmail.com>
2017-09-14 07:40:01 +0900

    Add router for core app, and connect it to the project urls

---
+++ ara/apps/core/views/router.py
@@ -0,0 +1,6 @@
+from rest_framework import routers
+
+from apps.core.views.viewsets import *
+
+
+router = routers.SimpleRouter()
--- ara/ara/urls.py
+++ ara/ara/urls.py
@@ -20,6 +20,7 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^', include('apps.core.urls', namespace='core')),
 ]
 
 
