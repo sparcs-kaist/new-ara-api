3a63b5f21a3b4e524b57ee0a7ca9213c3ed58e64
talkwithraon <talkwithraon@gmail.com>
2017-10-07 17:04:55 +0900

    remove VoteViewSet

--- ara/apps/core/permissions/vote.py
+++
@@ -1,6 +0,0 @@
-from rest_framework import permissions
-
-
-class VotePermission(permissions.IsAuthenticated):
-    def has_object_permission(self, request, view, obj):
-        return request.user.is_staff or request.user == obj.created_by
