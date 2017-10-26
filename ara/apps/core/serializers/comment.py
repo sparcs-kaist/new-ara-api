247312eda08e86ea17bd21c52f655744568faa2e
workwithraon <workwithraon@gmail.com>
2017-10-26 18:35:02 +0900

    Revert except handling

--- ara/apps/core/serializers/comment.py
+++ ara/apps/core/serializers/comment.py
@@ -12,12 +12,12 @@ class CommentSerializer(serializers.ModelSerializer):
 
         )
 
-    my_vote = serializers.SerializerMethodField()
     from apps.core.serializers.comment_log import CommentUpdateLogSerializer
-    comment_update_log_set = CommentUpdateLogSerializer(
-        many = True,
-    )
 
+    my_vote = serializers.SerializerMethodField()
+    comment_update_log_set = CommentUpdateLogSerializer(
+        many=True,
+    )
 
     def get_my_vote(self, obj):
         from apps.core.models import Vote
@@ -26,11 +26,9 @@ class CommentSerializer(serializers.ModelSerializer):
             return obj.vote_set.get(
                 created_by=self.context['request'].user,
             ).is_positive
-        except:
-            return None
 
-        #except Vote.DoesNotExist:
-        #    return None
+        except Vote.DoesNotExist:
+            return None
 
 
 class CommentCreateActionSerializer(serializers.ModelSerializer):
