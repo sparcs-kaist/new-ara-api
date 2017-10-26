1917cd6228c50b3762302547433e0af568c272d0
Dongkwan Kim <todoaskit@gmail.com>
2017-10-26 18:59:55 +0900

    Start session app

--- ara/ara/urls.py
+++ ara/ara/urls.py
@@ -21,6 +21,7 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('apps.core.urls', namespace='core')),
+    url(r'^', include('apps.session.urls', namespace='session')),
 ]
 
 
