c5503fe1020f2c09b7024067ed70b4416ed27cb5
talkwithraon <talkiwthraon@gmail.com>
2018-01-22 04:44:58 +0900

    Add api-token-refresh endpoint

--- ara/ara/urls.py
+++ ara/ara/urls.py
@@ -27,8 +27,12 @@ urlpatterns = [
 
 # installed apps (real environment)
 
+from rest_framework_jwt.views import refresh_jwt_token
+
+
 urlpatterns += [
     url(r'^rest-auth/', include('rest_auth.urls')),
+    url(r'^rest-auth/api-token-refresh/', refresh_jwt_token),
 ]
 
 
