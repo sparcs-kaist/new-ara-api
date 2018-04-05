c147926756292f472040587a6b1745c20fe5f269
talkwithraon <talkwithraon@gmail.com>
2018-04-05 15:46:02 +0900

    Bug fix: change field name from 'created_by' to 'user'

--- ara/apps/session/permissions/user_profile.py
+++ ara/apps/session/permissions/user_profile.py
@@ -3,4 +3,4 @@ from rest_framework import permissions
 
 class UserProfilePermission(permissions.IsAuthenticated):
     def has_object_permission(self, request, view, obj):
-        return request.user.is_staff or request.user == obj.created_by
+        return request.user.is_staff or request.user == obj.user
