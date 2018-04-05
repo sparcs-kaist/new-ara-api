fce940c3270f46fa72048a90ca86a6df74c247f7
talkwithraon <talkwithraon@gmail.com>
2018-04-05 15:48:23 +0900

    Change UserProfile model to MetaDataModel and add migration files.

--- ara/apps/session/models.py
+++ ara/apps/session/models.py
@@ -2,45 +2,48 @@ from django.conf import settings
 from django.contrib.auth.models import User
 from django.db import models
 
+from ara.classes.model import MetaDataModel
+
 from apps.session.sparcssso import Client
 
 
-is_beta = [False, True][int(settings.SSO_IS_BETA)]
+is_beta = bool(int(settings.SSO_IS_BETA))
+
 sso_client = Client(settings.SSO_CLIENT_ID, settings.SSO_SECRET_KEY, is_beta=is_beta)
 
 
-class UserProfile(models.Model):
-    user = models.OneToOneField(
-        User,
-        related_name='profile',
-        verbose_name='사용자',
-    )
-
+class UserProfile(MetaDataModel):
     # SPARCS SSO spec
     sid = models.CharField(max_length=30)
 
     signature = models.TextField(
-        verbose_name='서명',
         blank=True,
+        verbose_name='서명',
     )
     see_sexual = models.BooleanField(
+        blank=True,
         default=False,
         verbose_name='성인/음란성 보기',
-        blank=True,
     )
     see_social = models.BooleanField(
+        blank=True,
         default=False,
         verbose_name='정치/사회성 보기',
-        blank=True,
     )
     user_nickname = models.CharField(
+        blank=True,
         max_length=128,
         verbose_name='닉네임',
-        blank=True,
     )
     profile_picture = models.ImageField(
-        verbose_name='프로필',
         blank=True,
+        verbose_name='프로필',
+    )
+
+    user = models.OneToOneField(
+        to=User,
+        related_name='profile',
+        verbose_name='사용자',
     )
 
     def __str__(self):
