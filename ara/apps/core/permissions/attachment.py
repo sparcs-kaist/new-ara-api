82d9ddaff335c3ccf51d61d502249c57e6fbc706
Zeniuus <jsh56son@kaist.ac.kr>
2018-04-07 23:31:46 +0900

    Set permission of APIs of Attachment model

---
+++ ara/apps/core/permissions/attachment.py
@@ -0,0 +1,10 @@
+from rest_framework import permissions
+
+
+class AttachmentPermission(permissions.IsAuthenticated):
+    def has_object_permission(self, request, view, obj):
+        if request.method == 'DELETE':
+            return request.user.is_staff or all([request.user == article.created_by
+                                                 for article in obj.article_set.all()])
+
+        return super(AttachmentPermission, self).has_object_permission(request, view, obj)
--- ara/apps/core/views/viewsets/attachment.py
+++ ara/apps/core/views/viewsets/attachment.py
@@ -4,10 +4,14 @@ from ara.classes.viewset import ActionAPIViewSet
 
 from apps.core.models import Attachment
 from apps.core.serializers.attachment import AttachmentSerializer
+from apps.core.permissions.attachment import AttachmentPermission
 
 
 class AttachmentViewSet(mixins.CreateModelMixin,
                         mixins.DestroyModelMixin,
                         ActionAPIViewSet):
     queryset = Attachment.objects.all()
+    permission_classes = (
+        AttachmentPermission,
+    )
     serializer_class = AttachmentSerializer
