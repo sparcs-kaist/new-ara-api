94ea0b943105261f93db1cea72ed869e17bcfd39
talkwithraon <talkiwthraon@gmail.com>
2018-04-28 10:41:02 +0900

    Delete all legacy codes

--- ara/apps/core/views/viewsets/black_list.py
+++ ara/apps/core/views/viewsets/black_list.py
@@ -1,31 +1,19 @@
-from datetime import datetime
-
-from rest_framework import status, viewsets, response, decorators, serializers, permissions
+from rest_framework import viewsets
 
 from ara.classes.viewset import ActionAPIViewSet
 
 from apps.core.models import BlackList
-#from apps.core.filters.article import ArticleFilter
-#from apps.core.permissions.article import ArticlePermission
 from apps.core.serializers.black_list import BlackListSerializer, BlackListCreateActionSerializer
+
 
 class BlackListViewSet(viewsets.ModelViewSet, ActionAPIViewSet):
     queryset = BlackList.objects.all()
-    #filter_class = ArticleFilter
     serializer_class = BlackListSerializer
     action_serializer_class = {
         'create': BlackListCreateActionSerializer,
     }
-    #permission_classes = (
-    #    ArticlePermission,
-    #)
 
     def perform_create(self, serializer):
         serializer.save(
             black_from=self.request.user,
         )
-
-
-
-
-
