f5a5e7997f0cd6203950a4392ec7fdadb003c24b
Zeniuus <jsh56son@kaist.ac.kr>
2018-05-01 17:38:21 +0900

    Change best article logic
    Now, there are four kinds of best articles:
    1. daily bests - 5 articles with highest positive vote count
    2. daily bests - 5 articles with highest hit count
    3. weekly bests - 5 articles with highest positive vote count
    4. weekly bests - 5 articles with highest hit count

--- ara/apps/core/models/best_article.py
+++ ara/apps/core/models/best_article.py
@@ -3,6 +3,17 @@ from django.db import models
 from ara.classes.model import MetaDataModel
 
 
+PERIOD_CHOICES = (
+    ('daily', 'daily'),
+    ('weekly', 'weekly'),
+)
+
+BEST_BY_CHOICES = (
+    ('positive_vote_count', 'positive_vote_count'),
+    ('hit_count', 'hit_count')
+)
+
+
 class BestArticle(MetaDataModel):
     class Meta:
         verbose_name = '베스트 문서'
@@ -13,4 +24,18 @@ class BestArticle(MetaDataModel):
         db_index=True,
         related_name='best',
         verbose_name='문서',
+    )
+
+    period = models.CharField(
+        choices=PERIOD_CHOICES,
+        default='daily',
+        max_length=32,
+        verbose_name='베스트 문서 종류',
+    )
+
+    best_by = models.CharField(
+        choices=BEST_BY_CHOICES,
+        default='positive_vote_count',
+        max_length=32,
+        verbose_name="베스트 문서 선정 기준"
     )
--- ara/apps/core/views/home.py
+++ ara/apps/core/views/home.py
@@ -1,28 +1,38 @@
 from rest_framework import views, response
 
-from apps.core.models import BestArticle, BestComment, Board
+from apps.core.models import BestArticle, Board, PERIOD_CHOICES, BEST_BY_CHOICES
 from apps.core.serializers.article import ArticleSerializer
-from apps.core.serializers.comment import CommentSerializer
 from apps.core.serializers.board import BoardRecentArticleActionSerializer
 
 
 class HomeView(views.APIView):
     def get(self, request, format=None):
         return response.Response(data={
-            'best_articles': ArticleSerializer(
-                instance=[
-                    best_article.article for best_article in BestArticle.objects.order_by('-id')[:5]
-                ],
-                many=True,
-            ).data,
-            'best_comments': CommentSerializer(
-                instance=[
-                    best_comment.comment for best_comment in BestComment.objects.order_by('-id')[:5]
-                ],
-                many=True,
-            ).data,
+            'daily_bests': {
+                'positive_vote_count': _best_articles('daily', 'positive_vote_count'),
+                'hit_count': _best_articles('daily', 'hit_count'),
+            },
+            'weekly_bests': {
+                'positive_vote_count': _best_articles('weekly', 'positive_vote_count'),
+                'hit_count': _best_articles('weekly', 'hit_count'),
+            },
             'boards': BoardRecentArticleActionSerializer(
                 instance=Board.objects.prefetch_related('article_set'),
                 many=True,
             ).data,
         })
+
+
+def _best_articles(period, best_by):
+    try:
+        assert (period, period) in PERIOD_CHOICES and (best_by, best_by) in BEST_BY_CHOICES
+    except AssertionError:
+        raise ValueError(f'Wrong period or best_by: {period} / {best_by}')
+
+    return ArticleSerializer(
+        instance=[
+            best_article.article for best_article
+            in BestArticle.objects.filter(period=period, best_by=best_by).order_by('-id')[:5]
+        ],
+        many=True,
+    ).data
