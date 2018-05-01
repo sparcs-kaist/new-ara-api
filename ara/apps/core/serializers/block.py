9f03bfca2202d68700ebbbc2c934031f032a19b6
talkwithraon <talkiwthraon@gmail.com>
2018-05-01 17:11:29 +0900

    add meta data model's fields to read_only_fields

--- ara/apps/core/serializers/block.py
+++ ara/apps/core/serializers/block.py
@@ -7,6 +7,12 @@ class BlockSerializer(serializers.ModelSerializer):
     class Meta:
         model = Block
         fields = '__all__'
+        read_only_fields = (
+            'blocked_by',
+            'created_at',
+            'updated_at',
+            'deleted_at',
+        )
 
 
 class BlockCreateActionSerializer(serializers.ModelSerializer):
@@ -15,4 +21,7 @@ class BlockCreateActionSerializer(serializers.ModelSerializer):
         fields = '__all__'
         read_only_fields = (
             'blocked_by',
+            'created_at',
+            'updated_at',
+            'deleted_at',
         )
