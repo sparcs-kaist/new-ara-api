3b78199972f892e356225832d7ae342872168b9e
talkwithraon <talkwithraon@gmail.com>
2018-06-12 01:40:29 +0900

    replace get_or_create function to update_or_create.

--- ara/apps/core/views/viewsets/article.py
+++ ara/apps/core/views/viewsets/article.py
@@ -96,16 +96,13 @@ class ArticleViewSet(viewsets.ModelViewSet, ActionAPIViewSet):
     def retrieve(self, request, *args, **kwargs):
         article = self.get_object()
 
-        article_read_log, created = ArticleReadLog.objects.get_or_create(
+        article_read_log, created = ArticleReadLog.objects.update_or_create(
             read_by=self.request.user,
             article=article,
         )
 
         if created:
             article.update_hit_count()
-
-        else:
-            article_read_log.save()
 
         return super().retrieve(request, *args, **kwargs)
 
