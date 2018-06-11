86124a5e6f09ae47d103ea37ab0eb27882811039
talkwithraon <talkwithraon@gmail.com>
2018-06-12 03:10:54 +0900

    refactor serializers related to Article
    - add BaseArticleSerializer which contains all functions required.
    - ArticleSerializer and other action-related serializers are only cherry-picking fields.

--- ara/apps/core/serializers/article.py
+++ ara/apps/core/serializers/article.py
@@ -3,75 +3,15 @@ from rest_framework import serializers
 from apps.core.models import Article, ArticleReadLog, ArticleUpdateLog, Report, Vote
 from apps.core.serializers.article_log import ArticleUpdateLogSerializer
 from apps.core.serializers.board import BoardSerializer
-from apps.core.serializers.comment import CommentSerializer
+from apps.core.serializers.comment import Depth1CommentSerializer
 from apps.core.serializers.topic import TopicSerializer
 from apps.core.serializers.report import ReportSerializer
 
 
-class ArticleSerializer(serializers.ModelSerializer):
+class BaseArticleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Article
         fields = '__all__'
-
-    parent_topic = TopicSerializer()
-    parent_board = BoardSerializer()
-
-    created_by = serializers.SerializerMethodField()
-    read_status = serializers.SerializerMethodField()
-
-    def get_created_by(self, obj):
-        if obj.is_anonymous:
-            return '익명'
-
-        return obj.created_by.profile.nickname
-
-    def get_read_status(self, obj):
-        user = self.context['request'].user
-
-        try:
-            article_read_log = ArticleReadLog.objects.get(
-                article=obj,
-                read_by=user
-            )
-
-        except ArticleReadLog.DoesNotExist:
-            return 'N'
-
-        last_article_update_log = ArticleUpdateLog.objects.order_by('created_at').filter(
-            article=obj,
-        ).last()
-
-        if last_article_update_log:
-            if last_article_update_log.created_at > article_read_log.last_read_at:
-                return 'U'
-
-        if obj.commented_at:
-            if obj.commented_at > article_read_log.last_read_at:
-                return 'U'
-
-        return '-'
-
-
-class ArticleDetailActionSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Article
-        fields = '__all__'
-
-    my_vote = serializers.SerializerMethodField()
-    my_report = serializers.SerializerMethodField()
-    comments = serializers.SerializerMethodField()
-    article_current_page = serializers.SerializerMethodField()
-    created_by = serializers.SerializerMethodField()
-    read_status = serializers.SerializerMethodField()
-
-    parent_topic = TopicSerializer()
-    parent_board = BoardSerializer()
-    article_update_logs = ArticleUpdateLogSerializer(
-        many=True,
-        source='article_update_log_set',
-    )
-
-    list_serializer_class = ArticleSerializer
 
     def get_created_by(self, obj):
         if obj.is_anonymous:
@@ -80,7 +20,6 @@ class ArticleDetailActionSerializer(serializers.ModelSerializer):
         return obj.created_by.profile.nickname
 
     def get_my_vote(self, obj):
-
         try:
             return obj.vote_set.get(
                 voted_by=self.context['request'].user,
@@ -90,7 +29,6 @@ class ArticleDetailActionSerializer(serializers.ModelSerializer):
             return None
 
     def get_my_report(self, obj):
-
         try:
             return ReportSerializer(
                 instance=obj.report_set.get(
@@ -100,12 +38,6 @@ class ArticleDetailActionSerializer(serializers.ModelSerializer):
 
         except Report.DoesNotExist:
             return None
-
-    def get_comments(self, obj):
-        return CommentSerializer(
-            obj.comment_set.all(), many=True,
-            **{'context': {'request': self.context.get('request')}}
-        ).data
 
     def get_article_current_page(self, obj):
         view = self.context.get('view')
@@ -145,10 +77,59 @@ class ArticleDetailActionSerializer(serializers.ModelSerializer):
         return '-'
 
 
-class ArticleCreateActionSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Article
-        fields = '__all__'
+class ArticleSerializer(BaseArticleSerializer):
+    parent_topic = TopicSerializer(
+        read_only=True,
+    )
+    parent_board = BoardSerializer(
+        read_only=True,
+    )
+    comments = Depth1CommentSerializer(
+        many=True,
+        read_only=True,
+        source='comment_set',
+    )
+    article_update_logs = ArticleUpdateLogSerializer(
+        many=True,
+        read_only=True,
+        source='article_update_log_set',
+    )
+
+    my_vote = serializers.SerializerMethodField(
+        read_only=True,
+    )
+    my_report = serializers.SerializerMethodField(
+        read_only=True,
+    )
+    created_by = serializers.SerializerMethodField(
+        read_only=True,
+    )
+    read_status = serializers.SerializerMethodField(
+        read_only=True,
+    )
+    article_current_page = serializers.SerializerMethodField(
+        read_only=True,
+    )
+    
+
+class ArticleListActionSerializer(BaseArticleSerializer):
+    parent_topic = TopicSerializer(
+        read_only=True,
+    )
+    parent_board = BoardSerializer(
+        read_only=True,
+    )
+
+    created_by = serializers.SerializerMethodField(
+        read_only=True,
+    )
+    read_status = serializers.SerializerMethodField(
+        read_only=True,
+    )
+
+
+class ArticleCreateActionSerializer(ArticleSerializer):
+    class Meta(ArticleSerializer.Meta):
         read_only_fields = (
             'hit_count',
             'positive_vote_count',
@@ -161,10 +142,8 @@ class ArticleCreateActionSerializer(serializers.ModelSerializer):
         )
 
 
-class ArticleUpdateActionSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Article
-        fields = '__all__'
+class ArticleUpdateActionSerializer(ArticleSerializer):
+    class Meta(ArticleSerializer.Meta):
         read_only_fields = (
             'is_anonymous',
             'use_signature',
