748658ea0c40baf3cd9c433fd4aee163d7e0ffb2
talkwithraon <talkwithraon@gmail.com>
2018-06-13 19:22:26 +0900

    bugfix: commented_at is not saved when comment has parent_comment

--- ara/apps/core/models/signals/comment.py
+++ ara/apps/core/models/signals/comment.py
@@ -1,3 +1,5 @@
+import datetime
+
 from django.db import models
 from django.dispatch import receiver
 
@@ -6,14 +8,20 @@ from apps.core.models import Comment, Notification
 
 @receiver(models.signals.post_save, sender=Comment)
 def comment_post_save_signal(**kwargs):
-    import datetime
-
-    comment = kwargs['instance']
-    comment.parent_article.commented_at = datetime.datetime.now()
-    comment.parent_article.save()
-
     def notify_commented(comment):
         Notification.notify_commented(comment)
 
+    def update_article_commented_at(comment):
+        if comment.parent_article:
+            comment.parent_article.commented_at = datetime.datetime.now()
+            comment.parent_article.save()
+
+        else:
+            comment.parent_comment.parent_article.commented_at = datetime.datetime.now()
+            comment.parent_comment.parent_article.save()
+
+    comment = kwargs['instance']
+
     if kwargs['created']:
         notify_commented(comment)
+        update_article_commented_at(comment)
