e82b868c61ec70b0de89f365ce83a9631cd3f300
talkwithraon <talkwithraon@gmail.com>
2018-07-27 07:59:59 +0900

    serialize ScrapSerializer's parent_article field by using BaseArticleSerializer

--- ara/apps/core/serializers/scrap.py
+++ ara/apps/core/serializers/scrap.py
@@ -1,12 +1,15 @@
 from rest_framework import serializers
 
 from apps.core.models import Scrap
+from apps.core.serializers.article import BaseArticleSerializer
 
 
 class ScrapSerializer(serializers.ModelSerializer):
     class Meta:
         model = Scrap
         fields = '__all__'
+
+    parent_article = BaseArticleSerializer()
 
 
 class ScrapCreateActionSerializer(serializers.ModelSerializer):
