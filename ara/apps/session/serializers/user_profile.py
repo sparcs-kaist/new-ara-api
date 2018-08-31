7d9df90d5713ed1243f76bc14eb374cc642206d2
talkwithraon <talkwithraon@gmail.com>
2018-08-31 21:45:27 +0900

    add extra_preferences field to UserProfile related serializers

--- ara/apps/session/serializers/user_profile.py
+++ ara/apps/session/serializers/user_profile.py
@@ -3,30 +3,34 @@ from rest_framework import serializers
 from apps.session.models import UserProfile
 
 
-class UserProfileSerializer(serializers.ModelSerializer):
+class BaseUserProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserProfile
         fields = '__all__'
 
 
-class PublicUserProfileSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = UserProfile
+class UserProfileSerializer(BaseUserProfileSerializer):
+    extra_preferences = serializers.JSONField()
+
+
+class PublicUserProfileSerializer(BaseUserProfileSerializer):
+    class Meta(BaseUserProfileSerializer.Meta):
         fields = (
             'id',
             'picture',
             'nickname',
-
             'user'
         )
 
 
-class UserProfileUpdateActionSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = UserProfile
-        fields = (
-            'see_sexual',
-            'see_social',
-            'nickname',
-            'picture',
+class UserProfileUpdateActionSerializer(BaseUserProfileSerializer):
+    class Meta(BaseUserProfileSerializer.Meta):
+        read_only_fields = (
+            'sid',
+            'user',
+            'created_at',
+            'updated_at',
+            'deleted_at',
         )
+
+    extra_preferences = serializers.JSONField()
