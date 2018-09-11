ff025f77187d66b4e2ada583261c15770897e3d4
talkwithraon <talkwithraon@gmail.com>
2018-09-11 23:37:16 +0900

    add name field to Attachment model

--- ara/apps/core/models/attachment.py
+++ ara/apps/core/models/attachment.py
@@ -8,6 +8,9 @@ class Attachment(MetaDataModel):
         verbose_name = '첨부파일'
         verbose_name_plural = '첨부파일 목록'
 
+    name = models.TextField(
+        verbose_name='이름',
+    )
     file = models.FileField(
         verbose_name='링크',
     )
