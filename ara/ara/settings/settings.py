d5e4626465291259fa9da6220224de664c91144d
talkwithraon <talkwithraon@gmail.com>
2018-09-22 06:39:59 +0900

    remove useless settings

--- ara/ara/settings/settings.py
+++ ara/ara/settings/settings.py
@@ -146,16 +146,6 @@ INTERNAL_IPS = (
 )
 
 
-# https://docs.djangoproject.com/en/1.11/ref/settings/#login-url
-
-LOGIN_URL = '/user/login/'
-
-
-# https://docs.djangoproject.com/en/1.11/ref/settings/#logout-url
-
-LOGOUT_URL = '/user/logout/'
-
-
 # http://django-rest-auth.readthedocs.io/
 
 SITE_ID = 1
