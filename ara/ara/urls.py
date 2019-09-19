6a62a18eb19063ae4642f5731a5ff1d9bc09bb79
appleseed <apple12347678@gmail.com>
2019-09-19 13:44:57 +0900

    remove rest_auth as it is not used

--- ara/ara/urls.py
+++ ara/ara/urls.py
@@ -14,9 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.conf import settings
-from django.urls import path, include
 from django.contrib import admin
-
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,17 +23,7 @@ urlpatterns = [
     path('', include(('apps.user.urls', 'user'))),
 ]
 
-
-# installed apps (real environment)
-
-urlpatterns += [
-    path('api-auth/', include('rest_framework.urls')),
-    path('rest-auth/', include('rest_auth.urls')),
-]
-
-
 # installed apps (test environment)
-
 if settings.DEBUG:
     import debug_toolbar
     urlpatterns += [
