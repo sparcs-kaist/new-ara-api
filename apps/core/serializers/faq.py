dd25e023fec0e864043dfb76f06e8ac6e37e0692
Jooyon Kim <gina611.kaist@gmail.com>
2020-04-24 16:02:26 +0900

    Add FAQ serializer

---
+++ apps/core/serializers/faq.py
@@ -0,0 +1,12 @@
+from apps.core.models.faq import FAQ
+from ara.classes.serializers import MetaDataModelSerializer
+
+
+class BaseFAQSerializer(MetaDataModelSerializer):
+    class Meta:
+        model = FAQ
+        fields = '__all__'
+
+
+class FAQSerializer(BaseFAQSerializer):
+    pass
