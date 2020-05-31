4c9db6c579857c7910b1c1a76f8c1dcb94fc6646
busankimchi <jacky185@kaist.ac.kr>
2020-05-31 21:37:16 +0900

    Add Board testing

---
+++ tests/test_board.py
@@ -0,0 +1,19 @@
+import pytest
+from django.test import TestCase
+
+from apps.core.models import Board
+from tests.conftest import RequestSetting
+
+
+@pytest.mark.usefixtures('set_user_client')
+class TestBoard(TestCase, RequestSetting):
+
+    def test_list(self):
+        Board.objects.create(ko_name='자유 게시판',
+                             en_name='Free Board',
+                             ko_description='자유 게시판 입니다.',
+                             en_description='This is a free board.')
+
+        boards = self.http_request('get', 'boards')
+
+        assert boards.data.get('num_items') == 1
