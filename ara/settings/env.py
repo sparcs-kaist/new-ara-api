a40f579787a78408be2e4e094a4bf715b99728ff
victory-jooyon <gina611.kaist@gmail.com>
2020-07-16 01:35:17 +0900

    Enable settings env file to read environment variables without .env

--- ara/settings/env.py
+++ ara/settings/env.py
@@ -1,17 +1,29 @@
+import os
 import environ
-
+from os import environ as os_environ
 
 root = environ.Path(__file__) - 3
 
-environ.Env.read_env(env_file=root('.env'))
 env = environ.Env(
     DJANGO_ENV=(str, 'development'),
-    SECRET_KEY=(str),
-    DATABASE_URL=(str),
-    AWS_BUCKET_NAME=(str),
-    AWS_BUCKET_NAME_STATIC=(str),
-    AWS_ACCESS_KEY_ID=(str),
-    AWS_SECRET_ACCESS_KEY=(str),
-    SSO_CLIENT_ID=(str),
-    SSO_SECRET_KEY=(str)
+    SECRET_KEY=(str, os_environ.get('SECRET_KEY', 'QWEFdgRefhtYTdrhEt43rt4398tgtrghTewg5iorjehinht')),
+    AWS_BUCKET_NAME=(str, 'sparcs-kaist-ara-beta'),
+    AWS_BUCKET_NAME_STATIC=(str, 'sparcs-kaist-ara-beta-static'),
+    AWS_ACCESS_KEY_ID=(str, os_environ.get('AWS_ACCESS_KEY_ID')),
+    AWS_SECRET_ACCESS_KEY=(str, os_environ.get('AWS_SECRET_ACCESS_KEY')),
+    SSO_CLIENT_ID=(str, os_environ.get('SSO_CLIENT_ID')),
+    SSO_SECRET_KEY=(str, os_environ.get('SSO_SECRET_KEY'))
 )
+
+# For local environment
+if os.path.exists(root('.env')):
+    environ.Env.read_env(env_file=root('.env'))
+    env = environ.Env(
+        SECRET_KEY=(str),
+        AWS_BUCKET_NAME=(str),
+        AWS_BUCKET_NAME_STATIC=(str),
+        AWS_ACCESS_KEY_ID=(str),
+        AWS_SECRET_ACCESS_KEY=(str),
+        SSO_CLIENT_ID=(str),
+        SSO_SECRET_KEY=(str)
+    )
