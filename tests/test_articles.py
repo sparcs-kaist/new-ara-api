db230a92c9ca1d7f5a7a1e103a3cfe082f0ea352
JeesooYoon <jeesooyoon@kaist.ac.kr>
2020-07-27 19:38:43 +0900

    Add comment & make code pretty

--- tests/test_articles.py
+++ tests/test_articles.py
@@ -2,189 +2,188 @@ import pytest
 from django.test import TestCase
 from django.utils import timezone
 
-from apps.core.models import Vote, Article, ArticleReadLog, Topic, Board, Comment
+from apps.core.models import Article, ArticleReadLog, Topic, Board, Comment
 from tests.conftest import RequestSetting
 
 
-@pytest.mark.usefixtures('set_user_clients')
+@pytest.fixture(scope='class')
+def set_board(request):
+    request.cls.board = Board.objects.create(
+        slug="test board",
+        ko_name="테스트 게시판",
+        en_name="Test Board",
+        ko_description="테스트 게시판입니다",
+        en_description="This is a board for testing"
+    )
+
+
+@pytest.fixture(scope='class')
+def set_topic(request):
+    """set_board 먼저 적용"""
+    request.cls.topic = Topic.objects.create(
+        slug="test topic",
+        ko_name="테스트 토픽",
+        en_name="Test Topic",
+        ko_description="테스트용 토픽입니다",
+        en_description="This is topic for testing",
+        parent_board=request.cls.board
+    )
+
+
+@pytest.mark.usefixtures('set_user_clients', 'set_board', 'set_topic')
 class TestArticle(TestCase, RequestSetting):
+
+    # TODO: 밑의 test함수에서 create_article을 사용하려고 하니, create_article을 못찾겠다고 함. 계속 같은 코드를 쓰지 않기 위해서, 이 문제 해결해야함
+    def create_article(self):
+        Article.objects.create(
+            title="example article",
+            content="example content",
+            content_text="example content text",
+            is_anonymous=False,
+            is_content_sexual=False,
+            is_content_social=False,
+            hit_count=0,
+            positive_vote_count=0,
+            negative_vote_count=0,
+            created_by=self.user,
+            parent_topic=self.topic,
+            parent_board=self.board,
+            commented_at=timezone.now()
+        )
 
     # article 개수를 확인하는 테스트
     def test_list(self):
-        board = Board.objects.create(slug="board_example",
-                                     ko_name="게시판1",
-                                     en_name="board1",
-                                     ko_description="보드입니다",
-                                     en_description="This is board")
+        assert self.http_request('get', 'articles').data.get('num_items') == 0
 
-        topic = Topic.objects.create(slug="topic_example",
-                                     ko_name="토픽1",
-                                     en_name="topic1",
-                                     ko_description="토픽입니",
-                                     en_description="This is topic",
-                                     parent_board=board)
-
-        a = self.http_request('get', 'articles')
-        assert a.data.get('num_items') == 0
-
-        Article.objects.create(title="example article",
-                               content="example content",
-                               content_text="example content text",
-                               is_anonymous=True,
-                               is_content_sexual=False,
-                               is_content_social=False,
-                               hit_count=5,
-                               positive_vote_count=3,
-                               negative_vote_count=2,
-                               created_by=self.user,
-                               parent_topic=topic,
-                               parent_board=board,
-                               commented_at=timezone.now())
+        Article.objects.create(
+            title="example article",
+            content="example content",
+            content_text="example content text",
+            is_anonymous=False,
+            is_content_sexual=False,
+            is_content_social=False,
+            hit_count=0,
+            positive_vote_count=0,
+            negative_vote_count=0,
+            created_by=self.user,
+            parent_topic=self.topic,
+            parent_board=self.board,
+            commented_at=timezone.now()
+        )
 
         a = self.http_request('get', 'articles')
         assert a.data.get('num_items') == 1
 
-        Article.objects.create(title="example article",
-                               content="example content",
-                               content_text="example content text",
-                               is_anonymous=True,
-                               is_content_sexual=False,
-                               is_content_social=False,
-                               hit_count=5,
-                               positive_vote_count=3,
-                               negative_vote_count=2,
-                               created_by=self.user,
-                               parent_topic=topic,
-                               parent_board=board,
-                               commented_at=timezone.now())
+        Article.objects.create(
+            title="example article",
+            content="example content",
+            content_text="example content text",
+            is_anonymous=False,
+            is_content_sexual=False,
+            is_content_social=False,
+            hit_count=0,
+            positive_vote_count=0,
+            negative_vote_count=0,
+            created_by=self.user,
+            parent_topic=self.topic,
+            parent_board=self.board,
+            commented_at=timezone.now()
+        )
 
         a = self.http_request('get', 'articles')
         assert a.data.get('num_items') == 2
 
-        Article.objects.create(title="example article",
-                               content="example content",
-                               content_text="example content text",
-                               is_anonymous=True,
-                               is_content_sexual=False,
-                               is_content_social=False,
-                               hit_count=5,
-                               positive_vote_count=3,
-                               negative_vote_count=2,
-                               created_by=self.user,
-                               parent_topic=topic,
-                               parent_board=board,
-                               commented_at=timezone.now())
+        Article.objects.create(
+            title="example article",
+            content="example content",
+            content_text="example content text",
+            is_anonymous=False,
+            is_content_sexual=False,
+            is_content_social=False,
+            hit_count=0,
+            positive_vote_count=0,
+            negative_vote_count=0,
+            created_by=self.user,
+            parent_topic=self.topic,
+            parent_board=self.board,
+            commented_at=timezone.now()
+        )
 
         a = self.http_request('get', 'articles')
         assert a.data.get('num_items') == 3
 
     # article 생성이 잘 되는지 확인하는 테스트
     def test_create(self):
-        board = Board.objects.create(slug="board_example",
-                                     ko_name="게시판1",
-                                     en_name="board1",
-                                     ko_description="보드입니다",
-                                     en_description="This is board")
 
-        topic = Topic.objects.create(slug="topic_example",
-                                     ko_name="토픽1",
-                                     en_name="topic1",
-                                     ko_description="토픽입니다",
-                                     en_description="This is topic",
-                                     parent_board=board)
+        article = Article.objects.create(
+            title="example article",
+            content="example content",
+            content_text="example content text",
+            is_anonymous=False,
+            is_content_sexual=False,
+            is_content_social=False,
+            hit_count=0,
+            positive_vote_count=0,
+            negative_vote_count=0,
+            created_by=self.user,
+            parent_topic=self.topic,
+            parent_board=self.board,
+            commented_at=timezone.now()
+        )
 
-        article = Article.objects.create(title="example",
-                                         content="example content",
-                                         content_text="example content text",
-                                         is_anonymous=False,
-                                         is_content_sexual=False,
-                                         is_content_social=False,
-                                         hit_count=5,
-                                         positive_vote_count=3,
-                                         negative_vote_count=2,
-                                         created_by=self.user,
-                                         parent_topic=topic,
-                                         parent_board=board,
-                                         commented_at=timezone.now())
+        a = self.http_request('get', 'articles').data.get('results')[0]
 
-        a = self.http_request('get', 'articles')
-
-        a1 = a.data.get('results')[0]
-
-        assert a1.get('title') == article.title
-        assert a1.get('content') == article.content
-        assert a1.get('content_text') == article.content_text
-        assert a1.get('is_anonymous') == article.is_anonymous
-        assert a1.get('is_content_sexual') == article.is_content_sexual
-        assert a1.get('is_content_social') == article.is_content_social
-        assert a1.get('hit_count') == article.hit_count
-        assert a1.get('positive_vote_count') == article.positive_vote_count
-        assert a1.get('negative_vote_count') == article.negative_vote_count
-        assert a1.get('created_by')['username'] == self.user.username
-        assert a1.get('parent_topic')['ko_name'] == article.parent_topic.ko_name
-        assert a1.get('parent_board')['ko_name'] == article.parent_board.ko_name
+        assert a.get('title') == article.title
+        assert a.get('content') == article.content
+        assert a.get('content_text') == article.content_text
+        assert a.get('is_anonymous') == article.is_anonymous
+        assert a.get('is_content_sexual') == article.is_content_sexual
+        assert a.get('is_content_social') == article.is_content_social
+        assert a.get('hit_count') == article.hit_count
+        assert a.get('positive_vote_count') == article.positive_vote_count
+        assert a.get('negative_vote_count') == article.negative_vote_count
+        assert a.get('created_by')['username'] == self.user.username
+        assert a.get('parent_topic')['ko_name'] == article.parent_topic.ko_name
+        assert a.get('parent_board')['ko_name'] == article.parent_board.ko_name
 
     # 익명의 글쓴이가 익명임을 확인하는 테스트
     def test_anonymous_writer(self):
-        board = Board.objects.create(slug="board_example",
-                                     ko_name="게시판1",
-                                     en_name="board1",
-                                     ko_description="보드입니다",
-                                     en_description="This is board")
 
-        topic = Topic.objects.create(slug="topic_example",
-                                     ko_name="토픽1",
-                                     en_name="topic1",
-                                     ko_description="토픽입니다",
-                                     en_description="This is topic",
-                                     parent_board=board)
+        Article.objects.create(
+            title="example article",
+            content="example content",
+            content_text="example content text",
+            is_anonymous=True,
+            is_content_sexual=False,
+            is_content_social=False,
+            hit_count=0,
+            positive_vote_count=0,
+            negative_vote_count=0,
+            created_by=self.user,
+            parent_topic=self.topic,
+            parent_board=self.board,
+            commented_at=timezone.now()
+        )
 
-        article = Article.objects.create(title="example",
-                                         content="example content",
-                                         content_text="example content text",
-                                         is_anonymous=True,
-                                         is_content_sexual=False,
-                                         is_content_social=False,
-                                         hit_count=5,
-                                         positive_vote_count=3,
-                                         negative_vote_count=2,
-                                         created_by=self.user,
-                                         parent_topic=topic,
-                                         parent_board=board,
-                                         commented_at=timezone.now())
-
-        a = self.http_request('get', 'articles')
-        a1 = a.data.get('results')[0]
-        assert a1.get('created_by') == '익명'
+        assert self.http_request('get', 'articles').data.get('results')[0].get('created_by') == '익명'
 
     # hit_count, positive/negative votes, comments_count가 잘 업데이트 되는지 테스트
     def test_update_numbers(self):
-        board = Board.objects.create(slug="hi",
-                                     ko_name="게시판1",
-                                     en_name="board1",
-                                     ko_description="한글설명",
-                                     en_description="english testing")
-
-        topic = Topic.objects.create(slug="hi",
-                                     ko_name="한글이름",
-                                     en_name="e",
-                                     ko_description="한글설명",
-                                     en_description="d",
-                                     parent_board=board)
-
-        article = Article.objects.create(title="example",
-                                         content="example content",
-                                         content_text="example content text",
-                                         is_anonymous=True,
-                                         is_content_sexual=False,
-                                         is_content_social=False,
-                                         hit_count=0,
-                                         positive_vote_count=0,
-                                         negative_vote_count=0,
-                                         created_by=self.user,
-                                         parent_topic=topic,
-                                         parent_board=board,
-                                         commented_at=timezone.now())
+        article = Article.objects.create(
+            title="example article",
+            content="example content",
+            content_text="example content text",
+            is_anonymous=False,
+            is_content_sexual=False,
+            is_content_social=False,
+            hit_count=0,
+            positive_vote_count=0,
+            negative_vote_count=0,
+            created_by=self.user,
+            parent_topic=self.topic,
+            parent_board=self.board,
+            commented_at=timezone.now()
+        )
 
         a = self.http_request('get', 'articles').data.get('results')[0]
 
@@ -193,6 +192,7 @@ class TestArticle(TestCase, RequestSetting):
         # originally, hit_count 0
         assert a.get('hit_count') == 0
 
+        # TODO: HTTP request로 hit_count 증가하는 방법이 있을 것 같음. 직접 article read log 만들 필요 없을 것 같음. 이 다른 방법 찾기
         # creating ArticleReadLog increments hit_count by 1
         article_read_log = ArticleReadLog.objects.create(read_by=self.user2,
                                                          article=article)
@@ -206,6 +206,8 @@ class TestArticle(TestCase, RequestSetting):
 
         article_id_str = str(article.id)
 
+        # TODO: POST request로 vote_positive를 부를 때, 유저 정보 전달하는 방법 찾기
+        # 현재는 누적이 안되고 count가 0이나 1만 되어서, 계속 같은 유저로 들어가는 것 같음
         self.http_request('post', 'articles/' + article_id_str + '/vote_positive')
 
         a = self.http_request('get', 'articles').data.get('results')[0]
