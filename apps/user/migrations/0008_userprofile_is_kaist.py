6ca14bd54e0752ea52b9a40d30b47dda63fc720f
victory-jooyon <gina611.kaist@gmail.com>
2020-08-11 04:10:07 +0900

    Resolve migration conflicts

--- apps/user/migrations/0008_userprofile_is_kaist.py
+++ apps/user/migrations/0008_userprofile_is_kaist.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('user', '0005_userprofile_ara_id'),
+        ('user', '0007_userprofile_nickname_updated_at'),
     ]
 
     operations = [
