98e478dceb3c4c022e2ced81939c26285b5786c7
victory-jooyon <gina611.kaist@gmail.com>
2020-08-11 20:19:02 +0900

    Fix wrong db when making tests

--- ara/settings/test.py
+++ ara/settings/test.py
@@ -10,15 +10,19 @@ DEBUG = True
 TEST = True
 
 DATABASES = {
-    'default': { **env.db(),
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'charset': 'utf8mb4',
+        },
+        'NAME': os_environ.get('NEWARA_DB_NAME', 'new_ara'),
+        'USER': os_environ.get('NEWARA_DB_USER', 'root'),
+        'PASSWORD': os_environ.get('NEWARA_DB_PASSWORD', ''),
+        'HOST': os_environ.get('NEWARA_DB_HOST', 'localhost'),
+        'PORT': os_environ.get('NEWARA_DB_PORT', '3306'),
         'TEST': {
             'CHARSET': 'utf8mb4',
             'COLLATION': 'utf8mb4_general_ci',
         }
     }
 }
-
-try:
-    from .local_settings import *
-except ImportError:
-    pass
