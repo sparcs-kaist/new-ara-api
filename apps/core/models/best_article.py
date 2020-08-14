21f2dab2934ff966f30da21009f92a3730b37400
victory-jooyon <gina611.kaist@gmail.com>
2020-08-14 23:31:01 +0900

    Move multiple choice strings into model class

--- apps/core/models/best_article.py
+++ apps/core/models/best_article.py
@@ -3,18 +3,19 @@ from django.db import models
 from ara.db.models import MetaDataModel
 
 
-PERIOD_CHOICES = (
-    ('daily', 'daily'),
-    ('weekly', 'weekly'),
-)
-
-BEST_BY_CHOICES = (
-    ('positive_vote_count', 'positive_vote_count'),
-    ('hit_count', 'hit_count')
-)
-
-
 class BestArticle(MetaDataModel):
+    PERIOD_CHOICES_DAILY = 'daily'
+    PERIOD_CHOICES_WEEKLY = 'weekly'
+    PERIOD_CHOICES = (
+        (PERIOD_CHOICES_DAILY, PERIOD_CHOICES_DAILY),
+        (PERIOD_CHOICES_WEEKLY, PERIOD_CHOICES_WEEKLY),
+    )
+
+    BEST_BY_CHOICES_POSITIVE_VOTES = 'positive_vote_count'
+    BEST_BY_CHOICES = (
+        (BEST_BY_CHOICES_POSITIVE_VOTES, BEST_BY_CHOICES_POSITIVE_VOTES),
+    )
+
     class Meta(MetaDataModel.Meta):
         verbose_name = '베스트 문서'
         verbose_name_plural = '베스트 문서 목록'
