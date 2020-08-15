a5a825b6ca3ccb26c084344b827cc0dafed8a1c6
victory-jooyon <gina611.kaist@gmail.com>
2020-08-15 16:31:44 +0900

    Delete unneeded option in boolean field

--- apps/core/models/board.py
+++ apps/core/models/board.py
@@ -35,13 +35,11 @@ class Board(MetaDataModel):
     )
     is_kaist = models.BooleanField(
         verbose_name='카이스트 구성원 전용 게시판',
-        null=False,
         default=False
     )
     is_readonly = models.BooleanField(
         verbose_name='읽기 전용 게시판',
         help_text='활성화했을 때 관리자만 글을 쓸 수 있습니다. (ex. 포탈공지)',
-        null=False,
         default=False
     )
 
