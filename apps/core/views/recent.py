493662fa269d7b094f16603c8d281302efde137d
victory-jooyon <gina611.kaist@gmail.com>
2020-08-21 00:59:02 +0900

    Delete duplicate queries in recent view

--- apps/core/views/recent.py
+++ apps/core/views/recent.py
@@ -1,6 +1,6 @@
 from django.db.models import Case, When
 from rest_framework import views, response
-from apps.core.models import ArticleReadLog, datetime
+from apps.core.models import ArticleReadLog, Block, datetime
 from apps.core.serializers.article import ArticleListActionSerializer
 from ara.classes.pagination import PageNumberPagination
 
@@ -14,11 +14,19 @@ class RecentView(views.APIView):
 
 def recently_read_articles(request):
     paginator = PageNumberPagination()
-    recent_read_at = Case(When(updated_at=datetime.datetime.min, then='created_at'), default='updated_at')
-    recent_articles = ArticleReadLog.objects \
-        .filter(read_by=request.user) \
-        .annotate(recent_read_at=recent_read_at) \
-        .order_by('-recent_read_at')
+    recent_articles = ArticleReadLog.objects.filter(
+        read_by=request.user
+    ).order_by('-updated_at'
+               ).select_related('article', 'article__created_by',
+                                'article__created_by__profile',
+                                'article__parent_topic',
+                                'article__parent_board',
+                                ).prefetch_related('article__comment_set',
+                                                   'article__comment_set__comment_set',
+                                                   'article__attachments',
+                                                   'article__article_update_log_set',
+                                                   Block.prefetch_my_block(request.user, prefix='article__'))
+
     result_page = paginator.paginate_queryset(recent_articles, request)
 
     return ArticleListActionSerializer(
