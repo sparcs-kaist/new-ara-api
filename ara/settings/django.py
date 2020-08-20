251b6673da34f1c23c9e019d0a935b26a6f19612
victory-jooyon <gina611.kaist@gmail.com>
2020-08-20 18:16:07 +0900

    Upgrade django version to 3.1

--- ara/settings/django.py
+++ ara/settings/django.py
@@ -40,7 +40,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django_cookies_samesite.middleware.CookiesSameSite',
 ]
 
 ROOT_URLCONF = 'ara.urls'
