ddff9333ea24802aa80dce1fb36a9c46399a80ac
victory-jooyon <gina611.kaist@gmail.com>
2020-08-22 16:09:16 +0900

    Enable pagination control from client side

--- ara/classes/pagination.py
+++ ara/classes/pagination.py
@@ -4,13 +4,14 @@ from rest_framework import response, pagination
 
 
 class PageNumberPagination(pagination.PageNumberPagination):
-    page_size = 20
-    # page_size_query_param = 'page_size'
+    page_size_query_param = 'page_size'
 
     def get_paginated_response(self, data):
         return response.Response(OrderedDict([
             ('num_pages', self.page.paginator.num_pages),
             ('num_items', self.page.paginator.count),
             ('current', self.page.number),
+            ('previous', self.get_previous_link()),
+            ('next', self.get_next_link()),
             ('results', data)
         ]))
