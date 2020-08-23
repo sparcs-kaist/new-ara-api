1d2f43fc9c7eb4da080405888aad55879d8efbb2
Junghoon Noh <jungnoh.dev@gmail.com>
2020-08-23 09:35:24 +0900

    Rename i18n test functions

--- tests/test_i18n.py
+++ tests/test_i18n.py
@@ -5,7 +5,7 @@ from tests.conftest import RequestSetting
 
 
 @pytest.mark.usefixtures('set_user_client')
-class TestUserNickname(TestCase, RequestSetting):
+class TestInternationalization(TestCase, RequestSetting):
     def test_nickname_update(self):
         # 닉네임 변경시 nickname_updated_at 변경
         update_data = {
@@ -15,7 +15,6 @@ class TestUserNickname(TestCase, RequestSetting):
         r = self.http_request(self.user, 'put', f'user_profiles/{self.user.id}', data=update_data)
         self.user.refresh_from_db()
 
-        # 오류 발생
         update_data['nickname'] = 'bar'
         r = self.http_request(self.user, 'put', f'user_profiles/{self.user.id}', data=update_data, **{'HTTP_ACCEPT_LANGUAGE': 'ko'})
         assert r.data['nickname'][0].startswith('닉네임은 3개월마다 변경할 수 있습니다.')
