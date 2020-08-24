b3b75b17111501e0f52a56dad6eaacb5c639e673
Junghoon Noh <jungnoh.dev@gmail.com>
2020-08-24 22:07:14 +0900

    Add self-block validation, update created_at if block exists

--- apps/core/serializers/block.py
+++ apps/core/serializers/block.py
@@ -1,5 +1,6 @@
+from django.core.exceptions import ValidationError
 from django.utils.translation import gettext
-from rest_framework import serializers
+from django.utils import timezone
 
 from ara.classes.serializers import MetaDataModelSerializer
 
@@ -7,6 +8,11 @@ from apps.core.models import Block
 
 
 class BaseBlockSerializer(MetaDataModelSerializer):
+    def validate_user(self, value):
+        if self.context['request'].user and self.context['request'].user == value:
+            raise ValidationError(gettext('Cannot block yourself'))
+        return value
+
     class Meta:
         model = Block
         fields = '__all__'
@@ -29,3 +35,13 @@ class BlockCreateActionSerializer(BaseBlockSerializer):
         read_only_fields = (
             'blocked_by',
         )
+
+    def create(self, validated_data):
+        # 이미 차단 기록이 존재할 경우 IntegrityError 를 띄우지 않고 생성 시간만 변경하도록 함
+        block, _ = Block.objects.update_or_create(
+            **validated_data,
+            defaults={
+                'created_at': timezone.now()
+            }
+        )
+        return block
