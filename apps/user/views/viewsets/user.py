22ad87e827ee28bc438d7b1e243d46f5650dabf3
hbin0701 <hbin0701@kaist.ac.kr>
2020-08-25 20:38:26 +0900

    Add random default profile picture

--- apps/user/views/viewsets/user.py
+++ apps/user/views/viewsets/user.py
@@ -102,7 +102,38 @@ class UserViewSet(ActionAPIViewSet):
             user_profile.is_kaist = is_kaist
 
         except UserProfile.DoesNotExist:
+<<<<<<< HEAD
             user_nickname = _make_random_name()
+=======
+            nouns = ['외계인', '펭귄', '코뿔소', '여우', '염소', '타조', '사과', '포도', '다람쥐', '도토리', '해바라기', '코끼리', '돌고래', '거북이', '나비', '앵무새', '알파카', '강아지', '고양이', '원숭이', '두더지', '낙타', '망아지', '시조새', '힙스터', '로봇', '감자', '고구마', '가마우지', '직박구리', '오리너구리', '보노보', '개미핥기', '치타', '사자', '구렁이', '도마뱀', '개구리', '올빼미', '부엉이']
+            adjectives = ['부지런한', '즐거운', '열렬한', '유쾌한', '환호하는', '소심한', '빛나는', '열정적인', '유연한', '행복한', '활동적인', '용감한', '겸손한', '관대한', '따뜻한', '재미있는', '유능한', '예의바른', '생각하는',  '침착한', '태평한', '꼼꼼한', '정직한', '신중한', '창의적인', '가냘픈', '신나는', '귀여운', '기쁜', '귀찮은', '날랜', '바쁜', '듬직한', '사나운', '똑똑한', '더운', '추운', '징그러운', '젊은', '늙은']
+            random.shuffle(nouns)
+            random.shuffle(adjectives)
+            temp_nickname = adjectives[0] + ' ' + nouns[0]
+
+            colors = ["blue", "red", "gray"]
+            random.shuffle(colors)
+            numbers = ["1", "2", "3"]
+            random.shuffle(numbers)
+
+            temp_color = colors[0]
+            temp_num = numbers[0]
+            default_picture = f"user_profiles/default_pictures/{temp_color}-default{temp_num}.png"
+
+            try:
+                duplicate_user_profile = UserProfile.objects.get(
+                    nickname=temp_nickname,
+                )
+                tmparr = str(duplicate_user_profile.nickname).split(' ')
+                if len(tmparr) == 3:
+                    temp_nickname += ' ' + str(int(tmparr[-1]) + 1)
+                else:
+                    temp_nickname += ' 1'
+            except UserProfile.DoesNotExist:
+                pass
+
+
+>>>>>>> Add default profile picture in registration.
             with transaction.atomic():
                 new_user = get_user_model().objects.create_user(
                     email=user_info['email'],
@@ -116,7 +147,17 @@ class UserViewSet(ActionAPIViewSet):
                     nickname=user_nickname,
                     is_kaist=is_kaist,
                     sso_user_info=user_info,
+<<<<<<< HEAD
                     user=new_user,
+=======
+                    picture=default_picture,
+                    user=get_user_model().objects.create_user(
+                        email=user_info['email'],
+                        username=str(uuid.uuid4()),
+                        password=str(uuid.uuid4()),
+                        is_active=True if user_info.get('kaist_id') else False,
+                    ),
+>>>>>>> Add default profile picture in registration.
                 )
 
         if not user_profile.user.is_active:
