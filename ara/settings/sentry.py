e01009cd9822edb0c290692dccc633a258bea7a3
Junghoon Noh <jungnoh.dev@gmail.com>
2020-08-28 20:40:31 +0900

    Add sentry

---
+++ ara/settings/sentry.py
@@ -0,0 +1,13 @@
+import os
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
+sentry_sdk.init(
+    dsn=os.environ.get('SENTRY_DSN'),
+    integrations=[DjangoIntegration()],
+    traces_sample_rate=1.0,
+
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True
+)
