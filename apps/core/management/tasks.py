8ad67bbd9585119363829d23df551b997f39b663
victory-jooyon <gina611.kaist@gmail.com>
2020-08-31 10:41:49 +0900

    Fix cases with less than 5 articles voted for best articles

--- apps/core/management/tasks.py
+++ apps/core/management/tasks.py
@@ -37,10 +37,25 @@ def _get_best(days, period):
 
     hit_sorted = sorted(article_votes.items(), key=lambda x: article_hits[x[0]], reverse=True)
     articles = []
+    keys = []
+
+    length = len(article_votes)
     for key, _ in sorted(hit_sorted, key=lambda x: x[1], reverse=True)[:5]:
         articles.append(BestArticle(period=period,
                                     best_by=BestArticle.BEST_BY_CHOICES_POSITIVE_VOTES,
                                     article_id=key))
+        keys.append(key)
+
+    if length < 5:
+        for key, _ in sorted(article_hits, key=lambda x: x[1], reverse=True):
+            if key not in keys:
+                articles.append(BestArticle(period=period,
+                                            best_by=BestArticle.BEST_BY_CHOICES_POSITIVE_VOTES,
+                                            article_id=key))
+                keys.append(key)
+
+            if len(articles) >= 5:
+                break
 
     return BestArticle.objects.bulk_create(articles)
 
