bd68b9be309407b15f900ca5e110372144c36edb
victory-jooyon <gina611.kaist@gmail.com>
2020-09-21 01:00:00 +0900

    Fix home view accordingly with latest field

--- apps/core/views/home.py
+++ apps/core/views/home.py
@@ -1,29 +1,27 @@
 from rest_framework import views, response
 
-from apps.core.models import BestArticle, PERIOD_CHOICES, BEST_BY_CHOICES
+from apps.core.models import BestArticle, PERIOD_CHOICES
 from apps.core.serializers.article import BestArticleListActionSerializer
 
 
 class HomeView(views.APIView):
     def get(self, request):
         return response.Response(data={
-            'daily_bests': _best_articles('daily', 'positive_vote_count', request),
-            'weekly_bests': _best_articles('weekly', 'positive_vote_count', request),
+            'daily_bests': _best_articles('daily', request),
+            'weekly_bests': _best_articles('weekly', request),
         })
 
 
-def _best_articles(period, best_by, request):
+def _best_articles(period, request):
     try:
-        assert (period, period) in PERIOD_CHOICES and (
-            best_by, best_by) in BEST_BY_CHOICES
+        assert (period, period) in PERIOD_CHOICES
     except AssertionError:
-        raise ValueError(
-            'Wrong period or best_by: {} / {}'.format(period, best_by))
+        raise ValueError(f'Wrong period: {period}')
 
     return BestArticleListActionSerializer(
         instance=[
             best_article.article for best_article
-            in BestArticle.objects.filter(period=period, best_by=best_by).all()[:5]
+            in BestArticle.objects.filter(period=period, latest=True)
         ],
         many=True,
         **{'context': {'request': request}},
