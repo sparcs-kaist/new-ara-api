465d1c9e16d62f2d61efd3f5a533898686d8ff6a
victory-jooyon <gina611.kaist@gmail.com>
2020-09-21 23:26:29 +0900

    Merge migration files

--- apps/core/migrations/0019_delete_best_by_and_add_latest_for_best_articles.py
+++ apps/core/migrations/0019_delete_best_by_and_add_latest_for_best_articles.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('core', '0016_increase_filefield_max_length'),
+        ('core', '0018_auto_20200921_2306'),
     ]
 
     operations = [
