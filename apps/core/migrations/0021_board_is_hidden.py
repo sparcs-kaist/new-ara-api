d8d12d63beb61c697faf28ec83b7386fdf12c9b5
victory-jooyon <gina611.kaist@gmail.com>
2020-10-02 00:08:53 +0900

    Fix migration file related minor errors

--- apps/core/migrations/0021_board_is_hidden.py
+++ apps/core/migrations/0021_board_is_hidden.py
@@ -13,6 +13,6 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='board',
             name='is_hidden',
-            field=models.BooleanField(db_index=True, default=False, help_text='활성화했을 때 메인페이지 위 보드 리스트에 나타나지 않습니다. (ex. 뉴아라공지)', verbose_name='리스트 숨김 게시판'),
+            field=models.BooleanField(db_index=True, default=False, help_text='활성화했을 때 메인페이지 상단바 리스트에 나타나지 않습니다. (ex. 뉴아라공지)', verbose_name='리스트 숨김 게시판'),
         ),
     ]
