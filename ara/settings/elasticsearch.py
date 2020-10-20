3d13214f329e5bfae2b2362e9048960c1604f049
victory-jooyon <gina611.kaist@gmail.com>
2020-10-21 02:01:47 +0900

    Enable other elasticsearch ports, not only 9200

--- ara/settings/elasticsearch.py
+++ ara/settings/elasticsearch.py
@@ -1,11 +1,14 @@
 from os import environ as os_environ
+from elasticsearch_dsl import connections
 
 
 ELASTICSEARCH_HOST = os_environ.get('NEWARA_ELASTICSEARCH_HOST', 'localhost')
+ELASTICSEARCH_PORT = os_environ.get('NEWARA_ELASTICSEARCH_PORT', '9200')
+ELASTICSEARCH_URL = os_environ.get('NEWARA_ELASTICSEARCH_URL', f'{ELASTICSEARCH_HOST}:{ELASTICSEARCH_PORT}')
 
 ELASTICSEARCH_DSL = {
     'default': {
-        'hosts': f'{ELASTICSEARCH_HOST}:9200',
+        'hosts': ELASTICSEARCH_URL,
         'timeout': 30,
         'max_retries': 3,
         'retry_on_timeout': True,
