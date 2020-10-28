f111f8ba0f43fa01eeb7d0b4e3b322e3cbc4c252
victory-jooyon <gina611.kaist@gmail.com>
2020-10-29 00:23:51 +0900

    Move caching block from model to django signal

---
+++ apps/core/models/signals/block.py
@@ -0,0 +1,31 @@
+import time
+
+from django.db import models
+from django.dispatch import receiver
+from django.utils import timezone
+
+from apps.core.models import Block
+from ara import redis
+
+
+@receiver(models.signals.post_save, sender=Block)
+def block_post_save_signal(created, instance, **kwargs):
+    def add_block_to_redis(block):
+        pipe = redis.pipeline()
+        redis_key = f'blocks:{block.blocked_by_id}'
+        pipe.zadd(redis_key, {f'{block.user_id}': time.time()})
+        pipe.execute(raise_on_error=True)
+
+    def delete_block_from_redis(block):
+        pipe = redis.pipeline()
+        redis_key = f'blocks:{block.blocked_by_id}'
+        pipe.zrem(redis_key, f'{block.user_id}')
+        pipe.execute(raise_on_error=True)
+
+    deleted = instance.deleted_at != timezone.datetime.min.replace(tzinfo=timezone.utc)
+
+    if created:
+        add_block_to_redis(instance)
+
+    elif deleted:
+        delete_block_from_redis(instance)
