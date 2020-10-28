c80ccf97b108e68d29f51404ac0b8fd1157a44e0
victory-jooyon <gina611.kaist@gmail.com>
2020-10-28 18:40:55 +0900

    Add to redis when creating and deleting blocks

--- apps/core/models/block.py
+++ apps/core/models/block.py
@@ -1,6 +1,9 @@
+import time
+
 from django.db import models, IntegrityError
 from django.conf import settings
 
+from ara import redis
 from ara.db.models import MetaDataModel
 
 
@@ -37,6 +40,23 @@ class Block(MetaDataModel):
 
         super(Block, self).save(force_insert=force_insert, force_update=force_update, using=using, update_fields=update_fields)
 
+        created = force_insert
+        if created:
+            pipe = redis.pipeline()
+            redis_key = f'blocks:{self.blocked_by_id}'
+
+            pipe.zadd(redis_key, {f'{self.user_id}': time.time()})
+            pipe.execute(raise_on_error=True)
+
+    def delete(self, **kwargs):
+        super().delete(**kwargs)
+
+        pipe = redis.pipeline()
+        redis_key = f'blocks:{self.blocked_by_id}'
+        pipe.zrem(redis_key, f'{self.user_id}')
+
+        pipe.execute(raise_on_error=True)
+
     @classmethod
     def prefetch_my_block(cls, user, prefix=''):
         return models.Prefetch(
@@ -45,3 +65,8 @@ class Block(MetaDataModel):
                 blocked_by=user,
             ),
         )
+
+    @staticmethod
+    def is_blocked(blocked_by, user):
+        block_objs = redis.get_objs_by_values(f'blocks:{blocked_by.id}', 0, -1, (lambda x: int(x)))  # all blocks
+        return user.id in block_objs
