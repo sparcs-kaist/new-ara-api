5bc268fa8f6387c60c3177a130888ff47b2cf5de
victory-jooyon <gina611.kaist@gmail.com>
2020-10-30 02:02:06 +0900

    Replace using article update log to content_updated_at

--- tests/test_articles.py
+++ tests/test_articles.py
@@ -374,7 +374,12 @@ class TestArticle(TestCase, RequestSetting):
         # user1이 업데이트 (user2은 아직 변경사항 확인못함)
         self.http_request(self.user, 'get', f'articles/{self.article.id}')
         self.http_request(self.user, 'patch', f'articles/{self.article.id}', {'content': 'update!'})
+
+        # TODO: 현재는 프론트 구현상 게시물을 수정하면 바로 다시 GET을 호출하기 때문에 '-' 로 나옴.
+        #       추후 websocket 등으로 게시물 수정이 실시간으로 이루어진다면, 'U'로 나오기 때문에 수정 필요.
+        self.http_request(self.user, 'get', f'articles/{self.article.id}')
         res1 = self.http_request(self.user, 'get', 'articles')
-        res2 = self.http_request(self.user2, 'get', 'articles')
         assert res1.data['results'][0]['read_status'] == '-'
+
+        res2 = self.http_request(self.user2, 'get', 'articles')
         assert res2.data['results'][0]['read_status'] == 'U'
