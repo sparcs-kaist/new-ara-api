b739824d8bf40c8fb8caec22f8ae9caa28ae4173
victory-jooyon <gina611.kaist@gmail.com>
2020-10-30 22:58:39 +0900

    Add cascade deletion using django signal

---
+++ apps/core/models/signals/on_delete_cascade.py
@@ -0,0 +1,92 @@
+from django.contrib.auth import get_user_model
+from django.db import models
+from django.dispatch import receiver
+from django.utils import timezone
+
+from apps.core.models import Article, Attachment, Board, Comment, Notification, Topic
+
+
+# Core
+
+@receiver(models.signals.post_save, sender=Article)
+def cascade_soft_deletion_article(instance, **kwargs):
+    deleted = instance.deleted_at != timezone.datetime.min.replace(tzinfo=timezone.utc)
+
+    if deleted:
+        instance.article_read_log_set.all().update(deleted_at=instance.deleted_at)
+        instance.article_update_log_set.all().update(deleted_at=instance.deleted_at)
+        instance.article_delete_log_set.all().update(deleted_at=instance.deleted_at)
+        instance.best_set.all().update(deleted_at=instance.deleted_at)
+        instance.comment_set.all().update(deleted_at=instance.deleted_at)
+        instance.notification_set.all().update(deleted_at=instance.deleted_at)
+        instance.report_set.all().update(deleted_at=instance.deleted_at)
+        instance.scrap_set.all().update(deleted_at=instance.deleted_at)
+        instance.vote_set.all().update(deleted_at=instance.deleted_at)
+
+
+@receiver(models.signals.post_save, sender=Attachment)
+def cascade_soft_deletion_attachment(instance, **kwargs):
+    deleted = instance.deleted_at != timezone.datetime.min.replace(tzinfo=timezone.utc)
+
+    if deleted:
+        instance.comment_set.all().update(deleted_at=instance.deleted_at)
+
+
+@receiver(models.signals.post_save, sender=Board)
+def cascade_soft_deletion_board(instance, **kwargs):
+    deleted = instance.deleted_at != timezone.datetime.min.replace(tzinfo=timezone.utc)
+
+    if deleted:
+        instance.article_set.all().update(deleted_at=instance.deleted_at)
+        instance.topic_set.all().update(deleted_at=instance.deleted_at)
+
+
+@receiver(models.signals.post_save, sender=Comment)
+def cascade_soft_deletion_comment(instance, **kwargs):
+    deleted = instance.deleted_at != timezone.datetime.min.replace(tzinfo=timezone.utc)
+
+    if deleted:
+        instance.comment_set.all().update(deleted_at=instance.deleted_at)
+        instance.comment_update_log_set.all().update(deleted_at=instance.deleted_at)
+        instance.comment_delete_log_set.all().update(deleted_at=instance.deleted_at)
+        instance.notification_set.all().update(deleted_at=instance.deleted_at)
+        instance.report_set.all().update(deleted_at=instance.deleted_at)
+        instance.vote_set.all().update(deleted_at=instance.deleted_at)
+
+
+@receiver(models.signals.post_save, sender=Notification)
+def cascade_soft_deletion_notification(instance, **kwargs):
+    deleted = instance.deleted_at != timezone.datetime.min.replace(tzinfo=timezone.utc)
+
+    if deleted:
+        instance.notification_read_log_set.all().update(deleted_at=instance.deleted_at)
+
+
+@receiver(models.signals.post_save, sender=Topic)
+def cascade_soft_deletion_topic(instance, **kwargs):
+    deleted = instance.deleted_at != timezone.datetime.min.replace(tzinfo=timezone.utc)
+
+    if deleted:
+        instance.article_set.all().update(deleted_at=instance.deleted_at)
+
+
+# User
+
+
+@receiver(models.signals.post_delete, sender=get_user_model())
+def cascade_soft_deletion_user(instance, **kwargs):
+    now = timezone.now()
+
+    instance.article_set.all().update(deleted_at=now)
+    instance.article_read_log_set.all().update(deleted_at=now)
+    instance.article_update_log_set.all().update(deleted_at=now)
+    instance.article_delete_log_set.all().update(deleted_at=now)
+    instance.block_set.all().update(deleted_at=now)
+    instance.blocked_by_set.all().update(deleted_at=now)
+    instance.comment_set.all().update(deleted_at=now)
+    instance.comment_update_log_set.all().update(deleted_at=now)
+    instance.comment_delete_log_set.all().update(deleted_at=now)
+    instance.notification_read_log_set.all().update(deleted_at=now)
+    instance.report_set.all().update(deleted_at=now)
+    instance.scrap_set.all().update(deleted_at=now)
+    instance.vote_set.all().update(deleted_at=now)
