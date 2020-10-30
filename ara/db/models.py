98828b80a51241a2b3640807ed6c32984ab00a03
KangIn Kim <talkwithraon@gmail.com>
2020-10-31 00:39:34 +0900

    Update MetaDataQuerySet's delete function to use MetaDataCollector instead of Collector

--- ara/db/models.py
+++ ara/db/models.py
@@ -4,9 +4,34 @@ from django.utils import timezone
 
 class MetaDataQuerySet(models.QuerySet):
     def delete(self):
-        return super().update(**{
-            'deleted_at': timezone.now(),
-        })
+        """Delete the records in the current QuerySet."""
+        self._not_support_combined_queries('delete')
+        assert not self.query.is_sliced, \
+            "Cannot use 'limit' or 'offset' with delete."
+
+        if self._fields is not None:
+            raise TypeError("Cannot call delete() after .values() or .values_list()")
+
+        del_query = self._chain()
+
+        # The delete is actually 2 queries - one to find related objects,
+        # and one to delete. Make sure that the discovery of related
+        # objects is performed on the same database as the deletion.
+        del_query._for_write = True
+
+        # Disable non-supported fields.
+        del_query.query.select_for_update = False
+        del_query.query.select_related = False
+        del_query.query.clear_ordering(force_empty=True)
+
+        # Changed in MetaDataQuerySet: collector = Collector(using=del_query.db)
+        collector = MetaDataCollector(using=del_query.db)
+        collector.collect(del_query)
+        deleted, _rows_count = collector.delete()
+
+        # Clear the result cache, in case this QuerySet gets reused.
+        self._result_cache = None
+        return deleted, _rows_count
 
     def hard_delete(self):
         return super().delete()
