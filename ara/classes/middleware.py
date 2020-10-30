38e1fd003446e76f068054826428e5aee0fb2140
KangIn Kim <talkwithraon@gmail.com>
2020-10-30 14:19:24 +0900

    Add cors related http header

--- ara/classes/middleware.py
+++ ara/classes/middleware.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.conf import settings
 from django.urls import resolve
 
 
@@ -14,12 +14,15 @@ class CheckTermsOfServiceMiddleware:
         self.get_response = get_response
 
     def __call__(self, request):
+        response = self.get_response(request)
+
         if resolve(request.path_info).url_name not in self.ALLOWED_URL_NAMES and \
            (request.user.is_authenticated and request.user.profile.agree_terms_of_service_at is None):
-            return HttpResponse(
-                status=418,  # Use unusual http status code for avoiding conflict
-            )
+            if getattr(settings, 'CORS_ORIGIN_ALLOW_ALL', False):
+                response['Access-Control-Allow-Origin'] = '*'
 
-        response = self.get_response(request)
+            response.status_code = 418  # Use unusual http status code for avoiding conflict
+
+            return response
 
         return response
