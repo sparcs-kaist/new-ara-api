922ddcd2ad7b7f8bd46caffe9f0ec35e444bb2b2
KangIn Kim <talkwithraon@gmail.com>
2020-10-30 16:51:39 +0900

    Remove response content

--- ara/classes/middleware.py
+++ ara/classes/middleware.py
@@ -17,6 +17,7 @@ class CheckTermsOfServiceMiddleware:
 
         if resolve(request.path_info).url_name not in self.ALLOWED_URL_NAMES and \
            (request.user.is_authenticated and request.user.profile.agree_terms_of_service_at is None):
+            response.content = ''
             response.status_code = 418  # Use unusual http status code for avoiding conflict
 
             return response
