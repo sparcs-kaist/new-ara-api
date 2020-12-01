e4cb412d3f07d321ff5168ac750ba3ae8f9ea731
KangIn Kim <talkwithraon@gmail.com>
2020-12-01 23:34:06 +0900

    Enable celery and redis integrations

--- ara/settings/sentry.py
+++ ara/settings/sentry.py
@@ -1,13 +1,20 @@
 import os
 import sentry_sdk
-from sentry_sdk.integrations.django import DjangoIntegration
+from sentry_sdk.integrations import (
+    celery,
+    django,
+    redis,
+)
 
 sentry_sdk.init(
     dsn=os.environ.get('SENTRY_DSN'),
-    integrations=[DjangoIntegration()],
+    integrations=[
+        celery.CeleryIntegration(),
+        django.DjangoIntegration(),
+        redis.RedisIntegration(),
+    ],
     traces_sample_rate=1.0,
-
     # If you wish to associate users to errors (assuming you are using
     # django.contrib.auth) you may enable sending PII data.
-    send_default_pii=True
+    send_default_pii=True,
 )
