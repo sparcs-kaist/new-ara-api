2782c389e7453140f054b0cb64373b01b03f4d63
KangIn Kim <talkwithraon@gmail.com>
2021-03-04 22:14:24 +0900

    Add InvalidSsoLoginView

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path, include
 
-from apps.core.views import HomeView, StatusView, router
+from apps.core.views import HomeView, InvalidSsoLoginView, StatusView, router
 
 urlpatterns = [
     path('api/', include(router.urls)),
     path('api/home/', view=HomeView.as_view(), name='HomeView'),
     path('api/status/', view=StatusView.as_view(), name='StatusView'),
+
+    path('invalid_sso_login/', InvalidSsoLoginView.as_view(), name='InvalidSsoLoginView'),
 ]
