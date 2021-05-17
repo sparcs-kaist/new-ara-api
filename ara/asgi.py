e99b69fa0a8d7eecde4a8676a6bf09cc68eeeda0
ivy <63594364+iv-y@users.noreply.github.com>
2021-05-17 12:51:53 +0000

    Add basic ProtocolTypeRouter (WIP)

--- ara/asgi.py
+++ ara/asgi.py
@@ -6,8 +6,10 @@ defined in the ASGI_APPLICATION setting.
 import os
 
 from django.core.asgi import get_asgi_application
-
+from channels.routing import ProtocolTypeRouter
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ara.settings')
 
-application = get_asgi_application()
+application = ProtocolTypeRouter({
+    'http': get_asgi_application(),
+})
