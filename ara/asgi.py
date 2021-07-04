ab29e2ba7c7e8c0a08914c89cca34d1a5792d128
ivy <63594364+iv-y@users.noreply.github.com>
2021-07-04 09:06:16 +0000

    Fix server configuration to daphne

--- ara/asgi.py
+++ ara/asgi.py
@@ -6,14 +6,19 @@ defined in the ASGI_APPLICATION setting.
 import os
 
 from django.core.asgi import get_asgi_application
+
+# Ensure django app is loaded before AuthMiddlewareStack, since it uses django ORM
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ara.settings')
+django_asgi_app = get_asgi_application()
+
+
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from apps.core.routing import websocket_urlpatterns
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ara.settings')
 
 application = ProtocolTypeRouter({
-    'http': get_asgi_application(),
+    'http': django_asgi_app,
     'websocket': AuthMiddlewareStack(
         URLRouter(websocket_urlpatterns)
     )
