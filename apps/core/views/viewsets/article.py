fee55c83c29a0905e5c6b202d3cd80c185777b39
Jeesoo Yoon <jeesooyoon@kaist.ac.kr>
2021-07-30 19:32:58 +0900

    Cleanup boolean conditions

--- apps/core/views/viewsets/article.py
+++ apps/core/views/viewsets/article.py
@@ -63,11 +63,8 @@ class ArticleViewSet(viewsets.ModelViewSet, ActionAPIViewSet):
 
         if self.action == 'list':
             created_by = self.request.query_params.get('created_by')
-            if created_by is not None:  # 특정 사용자의 글을 가져오는 쿼리
-                created_by = int(created_by)
-                if created_by != self.request.user.id:  # 사용자가 다른 사람의 글들을 가져올 때 (ex. 사용자 이름 클릭해서)
-                    # 다른 사용자의 글들을 볼때는, 익명 글 숨기기
-                    queryset = queryset.exclude(is_anonymous=1)
+            if created_by and int(created_by) != self.request.user.id:
+                queryset = queryset.exclude(is_anonymous=True)
 
             # optimizing queryset for list action
             queryset = queryset.select_related(
