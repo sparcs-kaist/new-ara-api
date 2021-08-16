8d5a8ae1628f5ecd5cba4b80c0ebb3c1cfce0b22
ivy <63594364+iv-y@users.noreply.github.com>
2021-08-16 13:27:37 +0000

    Unify websocket routing to default_endpoint

--- apps/core/routing.py
+++ apps/core/routing.py
@@ -3,6 +3,5 @@ from django.urls import re_path
 from .consumers import WebSocketHandler
 
 websocket_urlpatterns = [
-    re_path(r'ws/articles/$', WebSocketHandler.as_asgi()),
-    re_path(r'ws/articles/(?P<post_id>[0-9]+)/$', WebSocketHandler.as_asgi()),
+    re_path(r'ws/default_endpoint/$', WebSocketHandler.as_asgi()),
 ]
