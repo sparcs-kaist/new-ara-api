63080acfda0e32c2ff80685aeed9b71e86dace19
Jeesoo Yoon <jeesooyoon@kaist.ac.kr>
2021-08-28 02:36:24 +0900

    Add num_articles, num_comments, num_positive_votes to BaseUserProfileSerializer

--- apps/user/views/me.py
+++ apps/user/views/me.py
@@ -2,12 +2,11 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 
-from apps.user.models.user_profile import UserProfile
-from apps.user.serializers.user_profile import UserProfileSerializer
-
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import ensure_csrf_cookie
 
+from apps.user.models.user_profile import UserProfile
+from apps.user.serializers.user_profile import UserProfileSerializer
 
 class MeView(APIView):
     """
