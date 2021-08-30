d4d00af6a722ecf158682c38b2b39f18a21eec78
ivy <63594364+iv-y@users.noreply.github.com>
2021-08-30 10:51:40 +0000

    Specify board watching cases more precisely

--- apps/core/consumers.py
+++ apps/core/consumers.py
@@ -7,25 +7,28 @@ class WebSocketHandler(AsyncJsonWebsocketConsumer):
     # there is only one channel group
     groups = ['broadcast']
 
-    article_id_regex = re.compile(r'/post/(\d+)$')
+    article_id_regex = re.compile(r'^https://[a-z.]+/post/(\d+)/?(\?.*)?$')
+    board_regex = re.compile(r'^https://[a-z.]+/(board|archive)(/[a-z-]+)?/?(\?.*)?$')
 
     async def connect(self):
-        self.watching_article = 0
+        self.watching_article = -1
         await self.accept()
 
     async def disconnect(self, code):
         pass
 
     async def receive_json(self, content, **kwargs):
-        match = WebSocketHandler.article_id_regex.search(content['new_route'])
-        self.watching_article = int(match.group(1)) if match else 0
+        match = WebSocketHandler.article_id_regex.match(content['new_route'])
+        if match:
+            self.watching_article = int(match.group(1))
+        else:
+            self.watching_article = 0 if WebSocketHandler.board_regex.match(content['new_route']) else -1
 
-    # websocket sessions are not preserved between pages
     async def update_articlelist(self, event):
         """
         Notify if the client is watching the articlelist.
         """
-        if not self.watching_article:
+        if self.watching_article == 0:
             await self.send_json({
                 'msg_type': 'update',
                 'target': 'articlelist',
