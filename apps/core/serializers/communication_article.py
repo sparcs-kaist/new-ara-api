82df058391066072a50185a3fb44d7e9683cd156
Injoon Hwang <injoon0617@gmail.com>
2022-04-05 03:38:26 +0900

    Add all fields to CommunicationArticleSerializer

--- apps/core/serializers/communication_article.py
+++ apps/core/serializers/communication_article.py
@@ -1,12 +1,16 @@
+from rest_framework import serializers
+
 from ara.classes.serializers import MetaDataModelSerializer
 
 from apps.core.models.communication_article import CommunicationArticle
 
 
 class CommunicationArticleSerializer(MetaDataModelSerializer):
+    positive_vote_count = serializers.IntegerField(source='article.positive_vote_count')
+
     class Meta:
         model = CommunicationArticle
-        fields = []
-
+        fields = '__all__'
+    
     # TODO: is_confirmed_by_school 등을 timestamp 말고 progress_statues enum으로 바꿔서 프론트 보내주기
-    # 좋아요 30개 달설 전, 달성 후 확인 전, 확인 후 답변전, 답변 완료
+    #       좋아요 30개 달설 전, 달성 후 확인 전, 확인 후 답변전, 답변 완료
