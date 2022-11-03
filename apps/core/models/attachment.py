dd485681a7475e91e7637c72eb2c57a14419eb99
Injoon Hwang <injoon0617@gmail.com>
2022-11-04 00:54:16 +0900

    Use unique names for attachments

--- apps/core/models/attachment.py
+++ apps/core/models/attachment.py
@@ -1,15 +1,24 @@
+import os
+import uuid
+
 from django.db import models
 
 from ara.db.models import MetaDataModel
 
 
 class Attachment(MetaDataModel):
+    def _hash_filename(instance, filename):
+        s3_folder = "files/"
+        _, extension = os.path.splitext(filename)
+        unique_filename = str(uuid.uuid4()) + extension
+        return os.path.join(s3_folder, unique_filename)
+
     class Meta(MetaDataModel.Meta):
         verbose_name = "첨부파일"
         verbose_name_plural = "첨부파일 목록"
 
     file = models.FileField(
-        upload_to="files",
+        upload_to=_hash_filename,
         verbose_name="링크",
         max_length=200,
     )
