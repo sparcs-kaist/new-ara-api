c15faede883ff68a1588f4ee68a905d074bcc736
DoyunShin <doyun.shin@gmail.com>
2023-10-05 16:26:30 +0000

    refactor(type): fix articles in apps

--- apps/core/permissions/article.py
+++ apps/core/permissions/article.py
@@ -5,7 +5,7 @@ from apps.core.models.board import BoardAccessPermissionType
 
 
 class ArticlePermission(permissions.IsAuthenticated):
-    def has_object_permission(self, request, view, obj):
+    def has_object_permission(self, request, view, obj) -> bool:
         if request.method not in permissions.SAFE_METHODS:
             return request.user.is_staff or request.user == obj.created_by
         return super().has_object_permission(request, view, obj)
@@ -14,7 +14,7 @@ class ArticlePermission(permissions.IsAuthenticated):
 class ArticleReadPermission(permissions.BasePermission):
     message = "해당 게시물에 대한 읽기 권한이 없습니다."
 
-    def has_object_permission(self, request, view, obj: Article):
+    def has_object_permission(self, request, view, obj: Article) -> bool:
         return obj.parent_board.group_has_access_permission(
             BoardAccessPermissionType.READ, request.user.profile.group
         )
