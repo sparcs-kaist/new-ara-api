879cc8e0b9870a06d21ab9a528628f76e4db067a
Qndndn <hyooyh@sparcs.org>
2023-11-16 13:56:36 +0000

    fix: serializers

--- apps/calendar/serializers/calendar.py
+++ apps/calendar/serializers/calendar.py
@@ -1,12 +1,12 @@
-from calendar.models import Calendar, Tag
-
 from rest_framework import serializers
+
+from .models import Calendar, Tag
 
 
 class TagSerializer(serializers.ModelSerializer):
     class Meta:
         model = Tag
-        fields = ["name", "color", "calendar"]
+        fields = ["name", "color"]
 
 
 class CalendarSerializer(serializers.ModelSerializer):
@@ -15,3 +15,23 @@ class CalendarSerializer(serializers.ModelSerializer):
     class Meta:
         model = Calendar
         fields = "__all__"
+
+
+class RecursiveTagSerializer(serializers.Serializer):
+    def to_representation(self, value):
+        serializer = TagSerializer(value, context=self.context)
+        return serializer.data
+
+
+class RecursiveCalendarSerializer(serializers.Serializer):
+    def to_representation(self, value):
+        serializer = CalendarSerializer(value, context=self.context)
+        return serializer.data
+
+
+class TagSerializer(serializers.ModelSerializer):
+    calendars = RecursiveCalendarSerializer(many=True)
+
+    class Meta:
+        model = Tag
+        fields = ["name", "color", "calendars"]
