f79ebf3ac8169d4059fe1265adcebb7571af8911
yuwol <yuwol036@gmail.com>
2024-01-03 07:37:25 +0000

    feat(calendar): add endpoints

--- apps/calendar/urls.py
+++ apps/calendar/urls.py
@@ -1,12 +1,12 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .views.viewsets.calendar import CalendarViewSet, TagViewSet
+from .views import EventViewSet, TagViewSet
 
 router = DefaultRouter()
-router.register(r"calendars", CalendarViewSet, basename="calendar")
+router.register(r"events", EventViewSet, basename="event")
 router.register(r"tags", TagViewSet, basename="tag")
 
 urlpatterns = [
-    path("api/", include(router.urls)),
+    path("api/calendar/", include(router.urls)),
 ]
