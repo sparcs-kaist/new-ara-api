c46a07b0e8dfc113001f515cdfee04ff6768a08e
ddungiii <ggcc503@gmail.com>
2024-01-05 14:24:15 +0000

    refactor(global-notice): modify viewset to readonly

--- apps/global_notice/views.py
+++ apps/global_notice/views.py
@@ -2,21 +2,12 @@ from django.utils import timezone
 from rest_framework import permissions, viewsets
 
 from .models import GlobalNotice
-from .permissions import GlobalNoticePermission, IsGlobalNoticeAthenticated
 from .serializers import GlobalNoticeSerializer
 
 
-class GlobalNoticeViewSet(viewsets.ModelViewSet):
+class GlobalNoticeViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = GlobalNotice.objects.filter(
         started_at__lte=timezone.now(), expired_at__gte=timezone.now()
     )
     serializer_class = GlobalNoticeSerializer
-
-    permission_classes = (
-        IsGlobalNoticeAthenticated,
-        GlobalNoticePermission,
-    )
-    action_permission_classes = {
-        "create": (permissions.IsAuthenticated, GlobalNoticePermission)
-    }
     pagination_class = None
--- tests/test_global_notice.py
+++ tests/test_global_notice.py
@@ -6,7 +6,7 @@ from apps.global_notice.models import GlobalNotice
 from tests.conftest import RequestSetting, TestCase
 
 
-@pytest.mark.usefixtures("set_admin_client", "set_user_client")
+@pytest.mark.usefixtures("set_user_client")
 class TestGlobalNotice(TestCase, RequestSetting):
     N = 50
 
@@ -79,77 +79,3 @@ class TestGlobalNotice(TestCase, RequestSetting):
             self.user, "get", f"global_notices/{global_notice_not_started.id}"
         )
         assert res.status_code == status.HTTP_404_NOT_FOUND
-
-    def test_create(self):
-        notice_data = {
-            "ko_title": "글로벌 공지 제목",
-            "en_title": "global_notice title",
-            "ko_content": "글로벌 공지 본문",
-            "en_content": "global_notice content",
-            "started_at": timezone.now() - timezone.timedelta(days=1),
-            "expired_at": timezone.now() + timezone.timedelta(days=1),
-        }
-        res_user = self.http_request(self.user, "post", "global_notices", notice_data)
-        assert res_user.status_code == status.HTTP_403_FORBIDDEN
-
-        for _ in range(self.N):
-            res_admin = self.http_request(
-                self.admin, "post", "global_notices", notice_data
-            )
-            assert res_admin.status_code == status.HTTP_201_CREATED
-
-        assert GlobalNotice.objects.count() == self.N
-
-    def test_update(self):
-        global_notice = GlobalNotice.objects.create(
-            ko_title="글로벌 공지 제목",
-            en_title="global_notice title",
-            ko_content="글로벌 공지 본문",
-            en_content="global_notice content",
-            started_at=timezone.now() - timezone.timedelta(days=1),
-            expired_at=timezone.now() + timezone.timedelta(days=1),
-        )
-        new_ko_title = "새로운 제목"
-        new_ko_content = "새로운 본문"
-
-        res_user = self.http_request(
-            self.user,
-            "patch",
-            f"global_notices/{global_notice.id}",
-            {"ko_title": new_ko_title, "ko_content": new_ko_content},
-        )
-        assert res_user.status_code == status.HTTP_403_FORBIDDEN
-
-        res_admin = self.http_request(
-            self.admin,
-            "patch",
-            f"global_notices/{global_notice.id}",
-            {"ko_title": new_ko_title, "ko_content": new_ko_content},
-        )
-        assert res_admin.status_code == status.HTTP_200_OK
-        assert res_admin.data["ko_title"] == new_ko_title
-        assert res_admin.data["ko_content"] == new_ko_content
-
-    def test_destroy(self):
-        global_notice = GlobalNotice.objects.create(
-            ko_title="글로벌 공지 제목",
-            en_title="global_notice title",
-            ko_content="글로벌 공지 본문",
-            en_content="global_notice content",
-            started_at=timezone.now() - timezone.timedelta(days=1),
-            expired_at=timezone.now() + timezone.timedelta(days=1),
-        )
-        res_user = self.http_request(
-            self.user,
-            "delete",
-            f"global_notices/{global_notice.id}",
-        )
-        assert res_user.status_code == status.HTTP_403_FORBIDDEN
-
-        res_admin = self.http_request(
-            self.admin,
-            "delete",
-            f"global_notices/{global_notice.id}",
-        )
-        assert res_admin.status_code == status.HTTP_204_NO_CONTENT
-        assert GlobalNotice.objects.count() == 0
