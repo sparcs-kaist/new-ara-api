cb5219f68c159c77a7398ed1fd2a5055e661ec29
yuwol <yuwol036@gmail.com>
2024-01-06 06:53:25 +0000

    chore(global-notice): format codes

--- apps/global_notice/views.py
+++ apps/global_notice/views.py
@@ -1,5 +1,5 @@
 from django.utils import timezone
-from rest_framework import permissions, viewsets
+from rest_framework import viewsets
 
 from .models import GlobalNotice
 from .serializers import GlobalNoticeSerializer
@@ -7,7 +7,8 @@ from .serializers import GlobalNoticeSerializer
 
 class GlobalNoticeViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = GlobalNotice.objects.filter(
-        started_at__lte=timezone.now(), expired_at__gte=timezone.now()
+        started_at__lte=timezone.now(),
+        expired_at__gte=timezone.now(),
     )
     serializer_class = GlobalNoticeSerializer
     pagination_class = None
