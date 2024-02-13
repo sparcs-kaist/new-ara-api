4dab773aabf3a3996254ea50985a1df461a16f89
retroinspect <sangachoi@kaist.ac.kr>
2024-02-13 12:52:02 +0000

    test(fcm): add more util functions for mock fcm

--- ara/fcm.py
+++ ara/fcm.py
@@ -6,12 +6,6 @@ from firebase_admin import messaging
 
 
 class FCM:
-    Message = messaging.Message
-    Notification = messaging.Notification
-    WebpushNotificationAction = messaging.WebpushNotificationAction
-    WebpushConfig = messaging.WebpushConfig
-    WebpushNotification = messaging.WebpushNotification
-
     def subscribe_to_topic(self, tokens, sub):
         print("real: subscribe")
         messaging.subscribe_to_topic(tokens, sub)
@@ -20,26 +14,80 @@ class FCM:
         print("real: unsubscribe")
         messaging.unsubscribe_from_topic(tokens, sub)
 
-    def send(self, message):
+    def send(self, title="Title", body="Body", open_url="/", **kwargs):
         print("real: send")
-        messaging.send(message)
+        # This registration token comes from the client FCM SDKs.
+        # See documentation on defining a message payload.
+
+        message = fcm.Message(
+            notification=fcm.Notification(
+                title=title,
+                body=body,
+            ),
+            webpush=fcm.WebpushConfig(
+                notification=fcm.WebpushNotification(
+                    title=title,
+                    body=body,
+                    tag=open_url,
+                    renotify=True,
+                    icon="/img/icons/ara-pwa-192.png",
+                    actions=[
+                        fcm.WebpushNotificationAction("action_open", "Open"),
+                        fcm.WebpushNotificationAction("action_close", "Close"),
+                    ],
+                ),
+                # Maybe bug: fcm_options.link is not working
+            ),
+            data={"action_open_url": open_url},
+            **kwargs,
+        )
+
+        response = messaging.send(message)
+        # Response is a message ID string.
+
+        return response
+
+
+from collections import defaultdict
 
 
 class MockFCM(FCM):
     def __init__(self):
         self.journal = []
 
+    def get_params_dict(self, **kwargs):
+        args = defaultdict(lambda: None)
+        args.update(kwargs)
+        return args
+
     def subscribe_to_topic(self, tokens, sub):
         print("mock: subscribe")
-        self.journal.append({"name": "subscribe_to_topic", "params": [tokens, sub]})
+        self.journal.append(
+            {
+                "name": "subscribe_to_topic",
+                "params": self.get_params_dict(tokens=tokens, sub=sub),
+            }
+        )
 
     def unsubscribe_from_topic(self, tokens, sub):
         print("mock: unsubscribe")
-        self.journal.append({"name": "unsubscribe_from_topic", "params": [tokens, sub]})
+        self.journal.append(
+            {
+                "name": "unsubscribe_from_topic",
+                "params": self.get_params_dict(tokens=tokens, sub=sub),
+            }
+        )
 
-    def send(self, message):
+    def send(self, title="Title", body="Body", open_url="/", **kwargs):
         print("mock: send")
-        self.journal.append({"name": "send", "params": [message]})
+        self.journal.append(
+            {
+                "name": "send",
+                "params": self.get_params_dict(
+                    title=title, body=body, open_url=open_url, **kwargs
+                ),
+            }
+        )
 
     def call_count(self, name):
         return sum([item["name"] == name for item in self.journal])
--- ara/firebase.py
+++ ara/firebase.py
@@ -14,7 +14,7 @@ def fcm_unsubscrible(FCM_tokens, subs):  # TODO: fix typo
 
 def fcm_notify_topic(topic, title, body, open_url):
     try:
-        fcm_simple(title, body, open_url, topic=topic)
+        fcm.send(title, body, open_url, topic=topic)
     except Exception as e:
         print(e)
 
@@ -23,37 +23,6 @@ def fcm_notify_user(user, title, body, open_url):
     targets = FCMToken.objects.filter(user=user)
     for i in targets:
         try:
-            fcm_simple(title, body, open_url, token=i.token)
+            fcm.send(title, body, open_url, token=i.token)
         except:
             FCMToken.objects.filter(token=i.token).delete()
-
-
-def fcm_simple(title="Title", body="Body", open_url="/", **kwargs):
-    # This registration token comes from the client FCM SDKs.
-    # See documentation on defining a message payload.
-
-    message = fcm.Message(
-        notification=fcm.Notification(
-            title=title,
-            body=body,
-        ),
-        webpush=fcm.WebpushConfig(
-            notification=fcm.WebpushNotification(
-                title=title,
-                body=body,
-                tag=open_url,
-                renotify=True,
-                icon="/img/icons/ara-pwa-192.png",
-                actions=[
-                    fcm.WebpushNotificationAction("action_open", "Open"),
-                    fcm.WebpushNotificationAction("action_close", "Close"),
-                ],
-            ),
-            # Maybe bug: fcm_options.link is not working
-        ),
-        data={"action_open_url": open_url},
-        **kwargs,
-    )
-
-    response = fcm.send(message)
-    # Response is a message ID string.
