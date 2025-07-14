86cca376da60094a4b17c98b55d93c4c13d66206
SeungBeom-Park <127308007+Killerwhale-Park@users.noreply.github.com>
2025-07-14 17:08:43 +0900

    [SSO] Kaist_v2_info
    SSO에서 kaist v2 info만 넘어오는 경우를 대응합니다.

--- apps/user/models/user_profile.py
+++ apps/user/models/user_profile.py
@@ -136,6 +136,10 @@ class UserProfile(MetaDataModel):
         kaist_info = self.sso_user_info["kaist_info"]
         if kaist_info:
             return json.loads(kaist_info)["ku_kname"]
+        kaist_v2_info = self.sso_user_info["kaist_v2_info"]
+        if kaist_v2_info:
+            return json.loads(kaist_v2_info)["user_nm"]
+            
         return self.sso_user_info["last_name"] + self.sso_user_info["first_name"]
 
     @cached_property
