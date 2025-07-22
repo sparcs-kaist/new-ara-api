31da9a9c5d248f5562c94b6f900d64bad98770e6
SeungBeom-Park <killerwhale@sparcs.org>
2025-07-22 20:21:01 +0900

    add db migration file of new-notification system

--- apps/chatting/models/signals/message.py
+++ apps/chatting/models/signals/message.py
@@ -2,9 +2,9 @@ from django.db import models
 from django.dispatch import receiver
 
 from apps.chatting.models.message import ChatMessage
-from apps.core.models import Notification
 
 @receiver(models.signals.post_save, sender=ChatMessage)
 def chat_message_post_save_signal(instance, created, **kwargs):
     if created:
+        from apps.core.models import Notification # Local import to avoid circular import issues
         Notification.notify_message(instance)
