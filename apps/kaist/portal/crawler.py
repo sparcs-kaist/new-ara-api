3bb651a7cf6d602dda8a823f774f51b4516e8323
SeungBeom-Park <127308007+Killerwhale-Park@users.noreply.github.com>
2025-11-07 02:49:32 +0900

    fix get recent_post json parsing error
    .

--- apps/kaist/portal/crawler.py
+++ apps/kaist/portal/crawler.py
@@ -47,15 +47,14 @@ class Crawler:
     
     #post 사이의 링크가 끊긴 경우를 위해 현재를 기준으로 가장 최근 post id를 가져옵니다.
     @classmethod
-    def _get_recent_post_id(cls) -> int:
-        response : RecentPostListResponse = cls._session.get(
-            f"https://portal.kaist.ac.kr/wz/api/board/recents"
-        ).json()
-        #최신순으로 정렬
-        sorted = response.sort(key = lambda x : int(x['rnum']))
-        for post_item in sorted:
-            if post_item['delYn'] == 'N':
-                return post_item['pstNo']
+        resp = cls._session.get("https://portal.kaist.ac.kr/wz/api/board/recents")
+        payload = resp.json()  # 제공된 응답은 유효 JSON
+        items = payload["data"]
+        items_sorted = sorted(items, key=lambda x: int(x["rnum"]))
+
+        for post_item in items_sorted:
+            if post_item["delYn"] == "N":
+                return int(post_item["pstNo"])
 
     @classmethod
     def _parse_response(cls, res: PostResponse) -> Post:
