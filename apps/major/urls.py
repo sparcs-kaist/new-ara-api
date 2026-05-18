0b6b4334c8dcdaf737abb6f4bffe981a6095e29e
yuni0725 <yuni0725.kwon@gmail.com>
2026-05-18 20:54:38 +0900

    fix(major): Update urls.py

--- apps/major/urls.py
+++ apps/major/urls.py
@@ -7,5 +7,5 @@ from apps.user.views.me import MeView
 url_patterns = [path("api/", include(router.urls)),
                 path("api/majors/<int:major_id>/articles/", MajorArticleViewSet.as_view({"get": "list", "post": "create"}), name="major-article-list"),
                 path("api/majors/<int:major_id>/articles/<int:pk>/", MajorArticleViewSet.as_view({"get": "retrieve", "patch": "partial_update", "delete": "destroy"}), name="major-article-detail"),
-                path("api/majors/me", MeView.as_view(name="major-me")),]
+                path("api/majors/me/", MeView.as_view(name="major-me")),]
 
